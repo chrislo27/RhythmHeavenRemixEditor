6747d32e1f9adcd730a88077d42e74b77901d92a
chrislo27 <kinchrislo@gmail.com>
2016-11-02 20:26:31 -0700

    Add GSON equivalent

---
+++ core/src/chrislo27/rhre/json/GameObject.java
@@ -0,0 +1,52 @@
+package chrislo27.rhre.json;
+
+public class GameObject {
+
+	public String gameID;
+	public String gameName;
+
+	public SoundObject[] cues;
+
+	public PatternObject[] patterns;
+
+	public static class SoundObject {
+
+		public String id;
+		public String fileExtension = "ogg";
+
+		public String name;
+
+		public float duration = 0.5f;
+
+		public boolean canAlterPitch = false;
+		public boolean canAlterDuration = false;
+
+		public String introSound = null;
+
+		public float baseBpm = 120;
+
+		public boolean loops = false;
+	}
+
+	public static class PatternObject {
+
+		public String id;
+		public String name;
+
+		public boolean isStretchable = false;
+
+		public CueObject[] cues;
+
+		public static class CueObject {
+
+			public String id;
+			public float beat;
+
+			public float duration;
+
+			public int semitone;
+
+		}
+	}
+
+}
