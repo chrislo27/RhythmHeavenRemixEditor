9bbed2054c8bfc1cdee969fc1e6a7645e8cbd342
chrislo27 <kinchrislo@gmail.com>
2016-11-06 09:50:08 -0800

    Add selection palette

--- core/src/chrislo27/rhre/palette/Palettes.kt
+++ core/src/chrislo27/rhre/palette/Palettes.kt
@@ -6,16 +6,19 @@ abstract class AbstractPalette {
 
 	abstract val editorBg: Color
 
+	val selectionFill = Color(0.1f, 0.75f, 0.75f, 0.333f)
+	val selectionBorder = Color(0.1f, 0.85f, 0.85f, 1f)
+
 }
 
 class LightPalette : AbstractPalette() {
 
-	override val editorBg: Color = Color(0.95f, 0.95f, 0.95f, 1f)
+	override val editorBg = Color(0.95f, 0.95f, 0.95f, 1f)
 
 }
 
 class DarkPalette : AbstractPalette() {
 
-	override val editorBg: Color = Color(0.1f, 0.1f, 0.1f, 1f)
+	override val editorBg = Color(0.1f, 0.1f, 0.1f, 1f)
 
 }
