0056ca97463eaeadff0857f5987457ce6c11fc4c
chrislo27 <kinchrislo@gmail.com>
2016-12-07 15:29:22 -0800

    Bump version

--- core/src/chrislo27/rhre/Main.java
+++ core/src/chrislo27/rhre/Main.java
@@ -51,7 +51,7 @@ public class Main extends ionium.templates.Main {
 
 	@Override
 	public void create() {
-		Main.version = "v2.0.3";
+		Main.version = "v2.0.4";
 		GlobalVariables.versionUrl = "https://raw.githubusercontent.com/chrislo27/VersionPlace/master/RHRE-version" +
 				".txt";
 
