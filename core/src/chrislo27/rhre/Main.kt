b4189334df48ae4ec13683a2fe38437b821c010e
chrislo27 <kinchrislo@gmail.com>
2017-02-08 07:21:14 -0800

    stupid manifest

--- core/src/chrislo27/rhre/Main.kt
+++ core/src/chrislo27/rhre/Main.kt
@@ -60,7 +60,7 @@ class Main(l: Logger) : ionium.templates.Main(l) {
 	}
 
 	override fun create() {
-		ionium.templates.Main.version = "v2.3.0"
+		ionium.templates.Main.version = "v2.3.2"
 		GlobalVariables.versionUrl = null // Deprecated - use new versioning instead
 		VersionChecker
 
