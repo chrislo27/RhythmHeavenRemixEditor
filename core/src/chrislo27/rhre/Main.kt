269ad77f466b0e70e12baa468d2d15f11d5dcb65
chrislo27 <kinchrislo@gmail.com>
2017-02-12 13:51:58 -0800

    Bump version

--- core/src/chrislo27/rhre/Main.kt
+++ core/src/chrislo27/rhre/Main.kt
@@ -59,7 +59,7 @@ class Main(l: Logger) : ionium.templates.Main(l) {
 	}
 
 	override fun create() {
-		ionium.templates.Main.version = "v2.3.3-SNAPSHOT"
+		ionium.templates.Main.version = "v2.3.3"
 		GlobalVariables.versionUrl = null // Deprecated - use new versioning instead
 		VersionChecker
 
