b28075dc0af597af0d1ba22f50e9710cda55a2aa
chrislo27 <kinchrislo@gmail.com>
2017-02-19 15:26:49 -0800

    SNAPSHOT ver

--- core/src/chrislo27/rhre/Main.kt
+++ core/src/chrislo27/rhre/Main.kt
@@ -88,7 +88,7 @@ class Main(l: Logger) : ionium.templates.Main(l) {
 	}
 
 	override fun create() {
-		ionium.templates.Main.version = "v2.4.3-SNAPSHOT"
+		ionium.templates.Main.version = "v2.5.0-SNAPSHOT"
 		GlobalVariables.versionUrl = null // Deprecated - use new versioning instead
 		VersionChecker
 		AssetRegistry.instance().assetManager.setLoader(LazySound::class.java, LazySoundLoader(
