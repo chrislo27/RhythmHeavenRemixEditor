a1470edfb79e6928bb62fe4fd00c8b43243815fc
chrislo27 <kinchrislo@gmail.com>
2017-02-24 22:53:46 -0800

    Add specs

--- core/src/chrislo27/rhre/logging/SysOutPiper.kt
+++ core/src/chrislo27/rhre/logging/SysOutPiper.kt
@@ -1,5 +1,9 @@
 package chrislo27.rhre.logging
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import ionium.templates.Main
+import ionium.util.MemoryUtils
 import org.apache.commons.io.output.TeeOutputStream
 import java.io.File
 import java.io.FileOutputStream
@@ -31,6 +35,39 @@ object SysOutPiper {
 		file.createNewFile()
 
 		stream = FileOutputStream(file)
+
+		val ps = PrintStream(stream)
+		ps.println("==============\nAUTO-GENERATED\n==============\n")
+		val builder = StringBuilder()
+		builder.append("Game Specifications:\n")
+		builder.append("   Version: " + Main.version + "\n")
+		builder.append("   Application type: " + Gdx.app.type.toString() + "\n")
+
+		builder.append("\n")
+
+		builder.append("Operating System Specifications:\n")
+		builder.append("   Java Version: " + System.getProperty("java.version") + " " + System.getProperty(
+				"sun.arch.data.model") + " bit" + "\n")
+		builder.append("   OS Name: " + System.getProperty("os.name") + "\n")
+		builder.append("   OS Version: " + System.getProperty("os.version") + "\n")
+		builder.append("   JVM memory available: " + MemoryUtils.getMaxMemory() + " KB\n")
+
+		builder.append("\n")
+
+		builder.append("Processor Specifications:\n")
+		builder.append("   Cores: " + MemoryUtils.getCores() + "\n")
+
+		builder.append("\n")
+
+		builder.append("Graphics Specifications:\n")
+		builder.append("   Resolution: " + Gdx.graphics.width + "x" + Gdx.graphics.height + "\n")
+		builder.append("   Fullscreen: " + Gdx.graphics.isFullscreen + "\n")
+		builder.append("   GL_VENDOR: " + Gdx.gl.glGetString(GL20.GL_VENDOR) + "\n")
+		builder.append("   Graphics: " + Gdx.gl.glGetString(GL20.GL_RENDERER) + "\n")
+		builder.append("   GL Version: " + Gdx.gl.glGetString(GL20.GL_VERSION) + "\n")
+		ps.println(builder.toString())
+		ps.println("\n")
+		ps.flush()
 
 		newOut = TeeOutputStream(oldOut, stream)
 		newErr = TeeOutputStream(oldErr, stream)
