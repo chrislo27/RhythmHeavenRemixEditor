f65ef3eabe1b60b0e40cb2932f19c541710770ec
chrislo27 <kinchrislo@gmail.com>
2017-03-01 21:40:45 -0800

    SNAPSHOT ver

--- core/src/chrislo27/rhre/Main.kt
+++ core/src/chrislo27/rhre/Main.kt
@@ -106,7 +106,7 @@ class Main(l: Logger) : ionium.templates.Main(l) {
 	}
 
 	override fun create() {
-		ionium.templates.Main.version = "v2.6.1-SNAPSHOT"
+		ionium.templates.Main.version = "v2.7.0-SNAPSHOT"
 
 		SysOutPiper.pipe()
 
