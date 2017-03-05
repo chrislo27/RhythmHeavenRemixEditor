58e88c52b886804870fd42e9857fdd4dabd10470
chrislo27 <kinchrislo@gmail.com>
2017-03-04 22:39:05 -0800

    G translate edition

--- core/src/chrislo27/rhre/Main.kt
+++ core/src/chrislo27/rhre/Main.kt
@@ -121,7 +121,7 @@ class Main(l: Logger) : ionium.templates.Main(l) {
 				NamedLocale("English (UK)", Locale("en", "UK")),
 				NamedLocale("Français (French)", Locale("fr")),
 				NamedLocale("Español (Spanish)", Locale("es")),
-				NamedLocale("日本語 (Japanese)", Locale("ja"))
+				NamedLocale("日本語 (グーグル翻訳) (Japanese [Google Translate])", Locale("ja_GTRANSLATE"))
 															)
 		val languagesMap: Map<String, NamedLocale> = languagesList.associate { it.locale.toString() to it }
 
