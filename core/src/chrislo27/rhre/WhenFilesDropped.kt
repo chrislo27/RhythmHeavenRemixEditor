df4b006df71ca7323a0604d12058d239618d07da
chrislo27 <kinchrislo@gmail.com>
2017-03-09 21:31:26 -0800

    Remove redundant modifier

--- core/src/chrislo27/rhre/WhenFilesDropped.kt
+++ core/src/chrislo27/rhre/WhenFilesDropped.kt
@@ -1,11 +1,10 @@
 package chrislo27.rhre
 
 import com.badlogic.gdx.files.FileHandle
-import java.io.File
 
 
 interface WhenFilesDropped {
 
-	abstract fun onFilesDropped(list: List<FileHandle>)
+	fun onFilesDropped(list: List<FileHandle>)
 
 }
