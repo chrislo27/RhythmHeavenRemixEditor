3860ac712fb800945c5c9629c00966919ec5de0d
chrislo27 <kinchrislo@gmail.com>
2017-03-14 00:56:43 -0700

    Only 5 files aren't converted

---
+++ desktop/src/chrislo27/rhre/desktop/DesktopLauncher.kt
@@ -0,0 +1,47 @@
+package chrislo27.rhre.desktop
+
+import chrislo27.rhre.Main
+import chrislo27.rhre.lazysound.LazySound
+import com.badlogic.gdx.Files
+import com.badlogic.gdx.graphics.Color
+import ionium.desktop.ArgumentInferredLwjglAppConfig
+import ionium.desktop.GameLwjglApp
+import ionium.registry.GlobalVariables
+import ionium.util.Logger
+import java.util.*
+
+object DesktopLauncher {
+	private var logger: Logger? = null
+
+	@JvmStatic fun main(args: Array<String>) {
+		System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true")
+
+		logger = Logger("", com.badlogic.gdx.utils.Logger.DEBUG)
+		val main = Main(logger!!)
+
+		val config = ArgumentInferredLwjglAppConfig(args)
+		config.title = "Rhythm Heaven Remix Editor"
+		config.width = GlobalVariables.defaultWidth
+		config.height = GlobalVariables.defaultHeight
+		config.fullscreen = false
+		config.foregroundFPS = GlobalVariables.maxFps
+		config.backgroundFPS = GlobalVariables.maxFps
+		config.resizable = true
+		config.vSyncEnabled = true
+		config.samples = 0
+		config.initialBackgroundColor = Color.BLACK
+		config.allowSoftwareMode = true
+
+		config.inferFromArguments()
+
+		config.addIcon("images/icon/icon128.png", Files.FileType.Internal)
+		config.addIcon("images/icon/icon64.png", Files.FileType.Internal)
+		config.addIcon("images/icon/icon32.png", Files.FileType.Internal)
+		config.addIcon("images/icon/icon16.png", Files.FileType.Internal)
+
+		LazySound.forceLoadNow = Arrays.stream(args).anyMatch { s ->
+			s.equals("--force-load-lazy-sounds", ignoreCase = true)
+		}
+		GameLwjglApp(main, config, logger)
+	}
+}
