095b4504bed7b0f06c0e35f575a59dde0b150544
chrislo27 <kinchrislo@gmail.com>
2017-03-14 14:58:42 -0700

    Remove Italian credits because it's NYI

--- core/src/chrislo27/rhre/credits/Credits.kt
+++ core/src/chrislo27/rhre/credits/Credits.kt
@@ -9,9 +9,8 @@ object Credits {
 			"title" to "",
 			"programming" to "chrislo27",
 			"databasing" to "ahemtoday, Huebird of Happiness, GuardedLolz, chrislo27",
-			"localization" to """[DARK_GRAY]Español (Spanish)[]: Killble, quantic, GlitchyPSIX, David Mismo
-[DARK_GRAY]Français (French)[]: Gabgab2222, Pengu12345, Lovestep, Dragoneteur, chrislo27
-[DARK_GRAY]Italiano (Italian)[]: Huebird of Happiness""",
+			"localization" to """[RAINBOW]Español (Spanish)[]: Killble, quantic, GlitchyPSIX, David Mismo
+[RAINBOW]Français (French)[]: Gabgab2222, Pengu12345, Lovestep, Dragoneteur, chrislo27""",
 			"sfx" to "F Yeah, Rhythm Heaven! Tumblr, ahemtoday, Haydorf, megaminerzero, Chocolate2890, Whistler_420, TieSoul, Huebird of Happiness, GuardedLolz, TheRhythmKid, Kana, GlitchyPSIX, Mariofan5000",
 			"icons" to "ahemtoday, Whistler_420, Killble, TheNewOrchestra, Altonotone, Pengu12345, fartiliumstation, TheRhythmKid, Chowder, GlitchyPSIX",
 			"uidesign" to "GlitchyPSIX",
