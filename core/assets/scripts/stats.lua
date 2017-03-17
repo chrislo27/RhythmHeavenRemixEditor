14215b5bac9edf33940d09e4372e308ec69d186a
chrislo27 <kinchrislo@gmail.com>
2017-03-17 13:31:30 -0700

    Add series stats

--- core/assets/scripts/stats.lua
+++ core/assets/scripts/stats.lua
@@ -1,11 +1,11 @@
 -- RHRE2 games used outputter
 -- Prints out a list of all the games you used.
 
-print "Outputting games used"
-print()
+print "Outputting games used\n"
 
 do
     local out = ""
+	local seriesUsed = {}
 
     for k, v in pairs(remix.gamesUsed) do
 		local game = registry.games[v]
@@ -13,12 +13,24 @@ do
             out = out .. ("Nil game value? " .. tostring(v) .. ", ")
         else
             out = out .. (tostring(game.name) .. ", ")
+			if seriesUsed[game.series] == nil then
+				seriesUsed[game.series] = 0
+			end
+			seriesUsed[game.series] = tonumber(seriesUsed[game.series]) + 1
         end
     end
 
     print(out)
+	
+	do
+		print "\nGames from each series used:"
+		local seriesOut = ""
+		for k, v in pairs(seriesUsed) do
+			seriesOut = seriesOut .. k .. ": " .. tostring(v) .. "\n"
+		end
+		
+		print(seriesOut)
+	end
 end
 
-print()
-
-print "END OF OUTPUT"
+print "\nEND OF OUTPUT"
