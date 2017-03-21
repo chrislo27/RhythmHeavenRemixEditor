304260bd27af00aa10c8c77bc5fb57e05b31e539
chrislo27 <kinchrislo@gmail.com>
2017-03-20 23:30:26 -0700

    Bump version

--- core/src/chrislo27/rhre/Main.kt
+++ core/src/chrislo27/rhre/Main.kt
@@ -146,7 +146,7 @@ class Main(l: Logger) : ionium.templates.Main(l) {
 	}
 
 	override fun create() {
-		ionium.templates.Main.version = "v2.9.0-SNAPSHOT"
+		ionium.templates.Main.version = "v2.9.0"
 
 		SysOutPiper.pipe()
 
