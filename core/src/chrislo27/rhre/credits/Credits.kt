1e56614be8a0067cd2fc4bc268d3aecb0e99e8e6
chrislo27 <kinchrislo@gmail.com>
2017-05-06 17:47:28 -0700

    Update credits

--- core/src/chrislo27/rhre/credits/Credits.kt
+++ core/src/chrislo27/rhre/credits/Credits.kt
@@ -8,7 +8,7 @@ object Credits {
 	val list: List<Pair<String, String>> = listOf(
 			"title" to "",
 			"programming" to "chrislo27",
-			"databasing" to "ahemtoday, GuardedLolz, TieSoul, Huebird, ArendAlphaEagle, Armodillomatt12, eggman199, chrislo27",
+			"databasing" to "ahemtoday, GuardedLolz, TieSoul, Huebird, ArendAlphaEagle, Armodillomatt12, Eggman199, chrislo27",
 			"localization" to """[RAINBOW]Español (Spanish)[]: GlitchyPSIX, Killble, quantic, David Mismo, (◉.◉)☂
 [RAINBOW]Français (French)[]: Gabgab2222, Pengu12345, Lovestep, Dragoneteur, chrislo27""",
 			"sfx" to "F Yeah, Rhythm Heaven! Tumblr, ahemtoday, Haydorf, megaminerzero, Chocolate2890, Whistler_420, TieSoul, Huebird of Happiness, GuardedLolz, TheRhythmKid, Kana, GlitchyPSIX, Mariofan5000",
