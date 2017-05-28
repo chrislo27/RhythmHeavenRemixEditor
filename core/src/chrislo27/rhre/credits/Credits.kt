a034902e4a6b4c6aa6ad896116ba78c828028e93
chrislo27 <kinchrislo@gmail.com>
2017-05-28 13:04:46 -0700

    Update credits

--- core/src/chrislo27/rhre/credits/Credits.kt
+++ core/src/chrislo27/rhre/credits/Credits.kt
@@ -11,8 +11,8 @@ object Credits {
 			"databasing" to "ahemtoday, GuardedLolz, TieSoul, Huebird, ArendAlphaEagle, Armodillomatt12, Eggman199, chrislo27",
 			"localization" to """[RAINBOW]Español (Spanish)[]: GlitchyPSIX, Killble, quantic, David Mismo, (◉.◉)☂
 [RAINBOW]Français (French)[]: Gabgab2222, Pengu12345, Lovestep, Dragoneteur, chrislo27""",
-			"sfx" to "F Yeah, Rhythm Heaven! Tumblr, ahemtoday, Haydorf, megaminerzero, Chocolate2890, Whistler_420, TieSoul, Huebird of Happiness, GuardedLolz, TheRhythmKid, Kana, GlitchyPSIX, Mariofan5000, Miracle22",
-			"icons" to "ahemtoday, Whistler_420, Killble, TheNewOrchestra, Altonotone, Pengu12345, fartiliumstation, TheRhythmKid, Chowder, GlitchyPSIX",
+			"sfx" to "F Yeah, Rhythm Heaven! Tumblr, ahemtoday, Haydorf, megaminerzero, Chocolate2890, Whistler_420, TieSoul, Huebird of Happiness, Miracle22, GuardedLolz, TheRhythmKid, Kana, GlitchyPSIX, Mariofan5000",
+			"icons" to "ahemtoday, Whistler_420, Killble, TheNewOrchestra, Altonotone, Pengu12345, fartiliumstation, TheRhythmKid, Chowder, GlitchyPSIX, Miracle22",
 			"uidesign" to "GlitchyPSIX",
 			"misc" to "Pengu12345, ToonLucas22, Strawzzboy64, Chef May\nRhythm Heaven Discord Server",
 			"creditsgame" to "ahemtoday, Serena Strawberry, mistuh_salmon, chrislo27",
