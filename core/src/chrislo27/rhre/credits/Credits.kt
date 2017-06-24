2a594601027b8fa621c6b40e62f79ecb2f9c678a
chrislo27 <kinchrislo@gmail.com>
2017-06-23 22:00:19 -0700

    Add Unlockstep font

--- core/src/chrislo27/rhre/credits/Credits.kt
+++ core/src/chrislo27/rhre/credits/Credits.kt
@@ -9,7 +9,7 @@ object Credits {
 			"title" to "",
 			"programming" to "chrislo27",
 			"databasing" to "ahemtoday, GuardedLolz, TieSoul, Huebird, ArendAlphaEagle, Armodillomatt12, Eggman199, ChorusSquid, baguette, chrislo27",
-			"localization" to """[RAINBOW]Español (Spanish)[]: GlitchyPSIX, Killble, quantic, David Mismo, (◉.◉)☂
+			"localization" to """[RAINBOW]Español (Spanish)[]: GlitchyPSIX, Killble, quantic, David Mismo, (◉.◉)☂, TheGarnet_
 [RAINBOW]Français (French)[]: Gabgab2222, Pengu12345, Lovestep, Dragoneteur, chrislo27""",
 			"sfx" to "F Yeah, Rhythm Heaven! Tumblr, ahemtoday, Haydorf, megaminerzero, Chocolate2890, Whistler_420, TieSoul, Huebird of Happiness, Miracle22, GuardedLolz, TheRhythmKid, Kana, GlitchyPSIX, Mariofan5000",
 			"icons" to "ahemtoday, Whistler_420, Killble, TheNewOrchestra, Altonotone, Pengu12345, fartiliumstation, TheRhythmKid, Chowder, GlitchyPSIX, Miracle22",
@@ -21,6 +21,7 @@ KHttp
 Gson
 [#000007F]Lua[]J
 Apache Commons IO
+Unlockstep font
 [#FF8900]Kotlin[] by JetBrains
 [#B07219]Java[] by Oracle
 Rhythm Heaven assets by Nintendo""",
