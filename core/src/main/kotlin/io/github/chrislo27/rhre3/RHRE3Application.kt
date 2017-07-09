9f117ad5f15c90c2e699d69819b73eea5a740ba3
chrislo27 <kinchrislo@gmail.com>
2017-07-09 13:24:36 -0700

    good stuff

--- core/src/main/kotlin/io/github/chrislo27/rhre3/RHRE3Application.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/RHRE3Application.kt
@@ -3,6 +3,7 @@ package io.github.chrislo27.rhre3
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.files.FileHandle
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
 import io.github.chrislo27.toolboks.Toolboks
 import io.github.chrislo27.toolboks.ToolboksGame
@@ -12,6 +13,14 @@ import io.github.chrislo27.toolboks.logging.Logger
 
 class RHRE3Application(logger: Logger, logToFile: Boolean)
     : ToolboksGame(logger, logToFile, RHRE3.VERSION, Pair(1280, 720), false) {
+
+    private val defaultFontLargeKey = "default_font_large"
+    private val defaultBorderedFontLargeKey = "default_bordered_font_large"
+
+    val defaultFontLarge: BitmapFont
+        get() = fonts[defaultFontLargeKey].font!!
+    val defaultBorderedFontLarge: BitmapFont
+        get() = fonts[defaultBorderedFontLargeKey].font!!
 
     private val fontFileHandle: FileHandle by lazy { Gdx.files.internal("fonts/rodin.otf") }
     private val fontAfterLoadFunction: FreeTypeFont.() -> Unit = {
@@ -25,13 +34,21 @@ class RHRE3Application(logger: Logger, logToFile: Boolean)
     override fun create() {
         super.create()
         Toolboks.LOGGER.info("RHRE3 $version is starting...")
+
+        // font stuff
+        run {
+            fonts[defaultFontLargeKey] = createDefaultLargeFont()
+            fonts[defaultBorderedFontLargeKey] = createDefaultLargeBorderedFont()
+            fonts.loadUnloaded()
+        }
     }
 
     override fun postRender() {
         super.postRender()
 
         batch.begin()
-        defaultFont.draw(batch, "RHRE2 first pass", 50f, 50f)
+        defaultFont.draw(batch, "RHRE3 first pass", 50f, 50f)
+        defaultFontLarge.draw(batch, "RHRE3 first pass LARGE font", 50f, 500f)
         batch.end()
     }
 
@@ -55,6 +72,26 @@ class RHRE3Application(logger: Logger, logToFile: Boolean)
         return FreeTypeFont(fontFileHandle, emulatedSize, createDefaultTTFParameter()
                 .apply {
                     borderWidth = 1.5f
+                })
+                .setAfterLoad(fontAfterLoadFunction)
+    }
+
+    private fun createDefaultLargeFont(): FreeTypeFont {
+        return FreeTypeFont(fontFileHandle, emulatedSize, createDefaultTTFParameter()
+                .apply {
+                    size *= 4
+                    borderWidth *= 4
+                })
+                .setAfterLoad(fontAfterLoadFunction)
+    }
+
+    private fun createDefaultLargeBorderedFont(): FreeTypeFont {
+        return FreeTypeFont(fontFileHandle, emulatedSize, createDefaultTTFParameter()
+                .apply {
+                    borderWidth = 1.5f
+
+                    size *= 4
+                    borderWidth *= 4
                 })
                 .setAfterLoad(fontAfterLoadFunction)
     }
