e99d076c8aa1ced4f41bfbd699146e0091cb718b
chrislo27 <kinchrislo@gmail.com>
2017-07-09 11:11:31 -0700

    Add Credits

---
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/credits/Credits.kt
@@ -0,0 +1,29 @@
+package io.github.chrislo27.rhre3.credits
+
+
+object Credits {
+
+    val list: List<Pair<String, String>> = listOf(
+            "title" to "",
+            "programming" to "chrislo27",
+            "databasing" to "ahemtoday, GuardedLolz, Miracle22, TieSoul, Huebird, ArendAlphaEagle, Armodillomatt12, Eggman199, ChorusSquid, baguette, chrislo27",
+            "localization" to """[RAINBOW]Español (Spanish)[]: GlitchyPSIX, Killble, quantic, David Mismo, (◉.◉)☂, TheGarnet_
+[RAINBOW]Français (French)[]: Gabgab2222, Pengu12345, Lovestep, Dragoneteur, chrislo27""",
+            "sfx" to "F Yeah, Rhythm Heaven! Tumblr, ahemtoday, Haydorf, megaminerzero, Chocolate2890, Whistler_420, TieSoul, Huebird of Happiness, Miracle22, GuardedLolz, TheRhythmKid, Kana, GlitchyPSIX, Mariofan5000",
+            "icons" to "ahemtoday, Whistler_420, Killble, TheNewOrchestra, Altonotone, Pengu12345, fartiliumstation, TheRhythmKid, Chowder, GlitchyPSIX, Miracle22",
+            "uidesign" to "GlitchyPSIX",
+            "misc" to "Pengu12345, ToonLucas22, Strawzzboy64, Chef May, iRonnoc5, The Golden Station\nRhythm Heaven Discord Server",
+            "creditsgame" to "ahemtoday, Serena Strawberry, mistuh_salmon, chrislo27",
+            "technologies" to """[DARK_GRAY]lib[][#E10000]GDX[] by Badlogic Games, LWJGL
+KHttp
+Jackson (+ Afterburner, Kotlin modules)
+JGit
+Apache Commons IO
+[#FF8900]Kotlin[] by JetBrains
+[#B07219]Java[] by Oracle
+Rhythm Heaven assets by Nintendo""",
+            "you" to ""
+                                                 )
+    val sections: Map<String, String> = list.associate { it }
+
+}
