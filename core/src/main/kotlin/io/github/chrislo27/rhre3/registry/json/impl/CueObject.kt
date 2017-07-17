38e51a90f1074ba18b1c954a66b443603ebe6792
chrislo27 <kinchrislo@gmail.com>
2017-07-17 14:54:23 -0700

    Create POKOs

--- core/src/main/kotlin/io/github/chrislo27/rhre3/registry/json/impl/CueObject.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/registry/json/impl/CueObject.kt
@@ -1,9 +1,38 @@
 package io.github.chrislo27.rhre3.registry.json.impl
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonTypeName
+import io.github.chrislo27.rhre3.RHRE3
 import io.github.chrislo27.rhre3.registry.json.NamedIDObject
+import io.github.chrislo27.rhre3.registry.json.Verifiable
 
 @JsonTypeName("cue")
-class CueObject : NamedIDObject() {
+open class CueObject : NamedIDObject(), Verifiable {
 
+    var duration: Float = -1f
+
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    var stretchable: Boolean = false
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    var repitchable: Boolean = false
+
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    var fileExtension: String = "ogg"
+
+    override fun verify(): String? {
+        val builder = StringBuilder()
+
+        id
+        name
+
+        if (duration <= 0) {
+            builder.append("Duration $duration is negative")
+        }
+
+        if (fileExtension !in RHRE3.SUPPORTED_SOUND_TYPES) {
+            builder.append("File extension $fileExtension isn't a supported sound type (${RHRE3.SUPPORTED_SOUND_TYPES})")
+        }
+
+        return if (builder.isEmpty()) null else builder.toString()
+    }
 }
--- core/src/main/kotlin/io/github/chrislo27/rhre3/registry/json/impl/KeepTheBeatObject.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/registry/json/impl/KeepTheBeatObject.kt
@@ -1,13 +1,13 @@
 package io.github.chrislo27.rhre3.registry.json.impl
 
-import com.fasterxml.jackson.annotation.JsonTypeName
 import io.github.chrislo27.rhre3.registry.json.NamedIDObject
 import io.github.chrislo27.rhre3.registry.json.Verifiable
 import io.github.chrislo27.rhre3.registry.json.impl.pointer.CuePointerObject
 
-@JsonTypeName("pattern")
-class PatternObject : NamedIDObject(), Verifiable {
 
+class KeepTheBeatObject : NamedIDObject(), Verifiable {
+
+    var duration: Float = -1f
     lateinit var cues: List<CuePointerObject>
 
     override fun verify(): String? {
@@ -16,8 +16,12 @@ class PatternObject : NamedIDObject(), Verifiable {
         id
         name
 
+        if (duration <= 0) {
+            builder.append("Duration $duration is <= 0\n")
+        }
+
         if (cues.isEmpty()) {
-            builder.append("Cues array is empty")
+            builder.append("Cues array is empty\n")
         }
 
         return if (builder.isEmpty()) null else builder.toString()
