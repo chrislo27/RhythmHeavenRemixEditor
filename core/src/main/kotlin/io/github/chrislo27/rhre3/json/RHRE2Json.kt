3f381be8773f1d40050b39a75318fb19bc6e81a2
chrislo27 <kinchrislo@gmail.com>
2017-07-30 13:09:19 -0700

    Add RHRE2 save file definition POKO

---
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/json/RHRE2Json.kt
@@ -0,0 +1,68 @@
+package io.github.chrislo27.rhre3.json
+
+import com.badlogic.gdx.audio.Music
+import com.badlogic.gdx.files.FileHandle
+import com.badlogic.gdx.utils.Disposable
+import io.github.chrislo27.toolboks.version.Version
+
+class RemixObject {
+
+    @Transient var fileHandle: FileHandle? = null
+    @Transient var musicData: MusicData? = null
+
+    var musicAssociation: String? = null
+
+    var version: String? = null
+    val versionNumber: Int?
+        get() {
+            return Version.fromString(version ?: return null).numericalValue
+        }
+
+    var entities: MutableList<EntityObject>? = null
+
+    var playbackStart: Float = 0f
+    var musicVolume = 1f
+    var musicStartTime: Float = 0f
+    var bpmChanges: MutableList<BpmTrackerObject>? = null
+
+    var metadata = MetadataObject()
+
+    class EntityObject {
+
+        var id: String? = null
+        var beat: Float = 0f
+        var level: Int = 0
+
+        // optionals
+        var isPattern: Boolean = false
+        var width: Float = 0f
+        var semitone: Int = 0
+
+        var stopAlways: Boolean = false
+        var volume: Float = 1f
+
+    }
+
+    class BpmTrackerObject {
+
+        var beat: Float = 0f
+        var tempo: Float = 0f
+
+    }
+
+    class MetadataObject {
+
+        var author: String? = null
+        var description: String? = null
+        var gamesUsed: String? = null
+
+    }
+
+    data class MusicData(val music: Music, val file: FileHandle, val originalFileName: String) : Disposable {
+
+        override fun dispose() {
+            music.dispose()
+        }
+    }
+
+}
