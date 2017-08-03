b3019d144c7f2c40e13fb7b4d6d4c2d0f18a8d0f
chrislo27 <kinchrislo@gmail.com>
2017-08-03 16:06:28 -0700

    Add isAllInitialized

--- core/src/main/kotlin/io/github/chrislo27/rhre3/theme/Theme.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/theme/Theme.kt
@@ -1,5 +1,8 @@
 package io.github.chrislo27.rhre3.theme
 
+import com.badlogic.gdx.graphics.Color
+import io.github.chrislo27.toolboks.util.anyUninitializedLateinits
+
 
 abstract class Theme {
 
@@ -11,6 +14,32 @@ abstract class Theme {
                         RHRE0Theme()
                       )
     }
+
+    fun isAllInitialized(): Boolean {
+        return !(this.anyUninitializedLateinits() || trackers.anyUninitializedLateinits())
+    }
+
+    // main elements
+    @HexColor open lateinit var background: Color
+        protected set
+    @HexColor open lateinit var trackLine: Color
+        protected set
+
+    // trackers
+    open class Trackers {
+        @HexColor open lateinit var playback: Color
+            protected set
+        @HexColor open lateinit var musicStart: Color
+            protected set
+        @HexColor open lateinit var musicVolume: Color
+            protected set
+        @HexColor open lateinit var tempoChange: Color
+            protected set
+        @HexColor open lateinit var timeSignature: Color
+            protected set
+    }
+    lateinit open var trackers: Trackers
+        protected set
 
 }
 
