46548294748a56e80077f4ca8da82e7dbf7e9cfc
chrislo27 <kinchrislo@gmail.com>
2017-08-04 16:19:48 -0700

    Optimize BpmUtils

---
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/util/BpmUtils.kt
@@ -0,0 +1,58 @@
+package io.github.chrislo27.rhre3.util
+
+
+object BpmUtils {
+    fun beatsToSeconds(beat: Float, bpm: Float): Float =
+            beat / (bpm / 60)
+
+    fun secondsToBeats(seconds: Float, bpm: Float): Float =
+            seconds * (bpm / 60)
+}
+
+object Semitones {
+
+    const val SEMITONES_IN_OCTAVE = 12
+    const val SEMITONE_VALUE = 1f / SEMITONES_IN_OCTAVE
+
+    private val cachedPitches = mutableMapOf<Int, Float>()
+    private val keyNames = mutableMapOf(
+            0 to "C",
+            1 to "C#",
+            2 to "D",
+            3 to "D#",
+            4 to "E",
+            5 to "F",
+            6 to "F#",
+            7 to "G",
+            8 to "G#",
+            9 to "A",
+            10 to "A#",
+            11 to "B"
+                                       )
+    private val usedKeyNames = mutableMapOf<Int, String>().apply {
+        putAll(keyNames)
+    }
+
+    @JvmStatic
+    fun getSemitoneName(semitone: Int): String {
+        val abs = Math.abs(Math.floorMod(semitone, SEMITONES_IN_OCTAVE))
+        if (!usedKeyNames.containsKey(semitone)) {
+            usedKeyNames[semitone] = keyNames[abs]!! +
+                    if ((semitone >= 12 || semitone <= -1))
+                        (if (semitone > 0) "+" else "-").repeat(Math.abs(Math.floorDiv(semitone, SEMITONES_IN_OCTAVE)))
+                    else ""
+        }
+
+        return usedKeyNames[semitone]!!
+    }
+
+    @JvmStatic
+    fun getALPitch(semitone: Int): Float {
+        if (cachedPitches[semitone] == null) {
+            cachedPitches.put(semitone, Math.pow(2.0, (semitone * SEMITONE_VALUE).toDouble()).toFloat())
+        }
+
+        return cachedPitches[semitone] ?: 1f
+    }
+
+}
