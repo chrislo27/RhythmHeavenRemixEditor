462b6992d812d878fbbecb6a976222135f546388
chrislo27 <kinchrislo@gmail.com>
2017-08-10 12:54:35 -0700

    Add shell classes

---
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/entity/model/multipart/EquidistantEntity.kt
@@ -0,0 +1,13 @@
+package io.github.chrislo27.rhre3.entity.model.multipart
+
+import com.badlogic.gdx.math.Rectangle
+import io.github.chrislo27.rhre3.entity.model.MultipartEntity
+import io.github.chrislo27.rhre3.registry.datamodel.Equidistant
+import io.github.chrislo27.rhre3.track.Remix
+
+
+class EquidistantEntity(remix: Remix, datamodel: Equidistant) : MultipartEntity<Equidistant>(remix, datamodel) {
+    override fun updateInternalCache(old: Rectangle) {
+        TODO()
+    }
+}
---
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/entity/model/multipart/KeepTheBeatEntity.kt
@@ -0,0 +1,13 @@
+package io.github.chrislo27.rhre3.entity.model.multipart
+
+import com.badlogic.gdx.math.Rectangle
+import io.github.chrislo27.rhre3.entity.model.MultipartEntity
+import io.github.chrislo27.rhre3.registry.datamodel.KeepTheBeat
+import io.github.chrislo27.rhre3.track.Remix
+
+
+class KeepTheBeatEntity(remix: Remix, datamodel: KeepTheBeat) : MultipartEntity<KeepTheBeat>(remix, datamodel) {
+    override fun updateInternalCache(old: Rectangle) {
+        TODO()
+    }
+}
