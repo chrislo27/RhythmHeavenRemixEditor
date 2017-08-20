3535d5aa4b4569196e2cba9ceec9d1ac1dbc329e
chrislo27 <kinchrislo@gmail.com>
2017-08-20 10:52:27 -0700

    Add fast seeking music when starting at same position or ahead

--- core/src/main/kotlin/io/github/chrislo27/rhre3/track/music/MusicData.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/track/music/MusicData.kt
@@ -2,11 +2,11 @@ package io.github.chrislo27.rhre3.track.music
 
 import com.badlogic.gdx.files.FileHandle
 import io.github.chrislo27.rhre3.track.Remix
-import io.github.chrislo27.toolboks.util.GranularMusic
+import io.github.chrislo27.toolboks.util.FastSeekingMusic
 
 
 class MusicData(val handle: FileHandle, val remix: Remix) {
 
-    val music: GranularMusic = GranularMusic(handle)
+    val music: FastSeekingMusic = FastSeekingMusic(handle)
 
 }
