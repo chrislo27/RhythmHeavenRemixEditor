12d76b4338b47cb6c3089a8d051843b83a635377
chrislo27 <kinchrislo@gmail.com>
2017-08-26 21:04:18 -0700

    Add distribution files

---
+++ distribution/README.txt
@@ -0,0 +1,2 @@
+Please go to http://rhre.readthedocs.io/en/latest/README/.
+It has a properly formatted online readme that is always updated.
---
+++ distribution/run_windows.bat
@@ -0,0 +1,8 @@
+java -version
+
+@echo.
+@echo You NEED Java 1.8, at least. If the version above is not 1.8+, or if you get an UnsupportedClassVersionError, update Java.
+@echo.
+
+java -jar RHRE.jar
+pause
