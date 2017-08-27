01f0bbce5766298de6c99b30036fc5e90ed93b90
chrislo27 <kinchrislo@gmail.com>
2017-08-27 11:25:50 -0700

    Temp comment out sfx credits since there are none so far

--- core/src/main/kotlin/io/github/chrislo27/rhre3/credits/Credits.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/credits/Credits.kt
@@ -20,7 +20,7 @@ object Credits {
                 |
                 |[LIGHT_GRAY](Italian)[]
                 |Zenon""".trimMargin(),
-            "sfx" crediting "",
+//            "sfx" crediting "",
             "gfx" crediting "GlitchyPSIX",
             "specialThanks" crediting "baguette, GlitchyPSIX, Miracle22, Pengu123, TheRhythmKid, TheGarnet_, " +
                     "(◉.◉)☂, GrueKun, nerd, ChorusSquid, ArsenArsen, Clone5184, danthonywalker, jos",
