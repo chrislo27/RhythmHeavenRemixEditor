c72129051615566d442f9296cbc6f9f0507eba92
chrislo27 <kinchrislo@gmail.com>
2017-08-27 19:48:08 -0700

    Update credits

--- core/src/main/kotlin/io/github/chrislo27/rhre3/credits/Credits.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/credits/Credits.kt
@@ -17,7 +17,7 @@ object Credits {
                 |
                 |[LIGHT_GRAY](German)[]
                 |Zenon""".trimMargin(),
-//            "sfx" crediting "",
+            "sfx" crediting "baguette",
             "gfx" crediting "GlitchyPSIX",
             "specialThanks" crediting "baguette, GlitchyPSIX, Miracle22, Pengu123, TheRhythmKid, TheGarnet_, " +
                     "(◉.◉)☂, GrueKun, nerd, ChorusSquid, ArsenArsen, Clone5184, danthonywalker, jos",
