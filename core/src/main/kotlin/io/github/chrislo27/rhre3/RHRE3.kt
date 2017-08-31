c07c8057bcce43caf8b536090a2844ab6e2238be
chrislo27 <kinchrislo@gmail.com>
2017-08-30 22:23:17 -0700

    Bump version

--- core/src/main/kotlin/io/github/chrislo27/rhre3/RHRE3.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/RHRE3.kt
@@ -7,7 +7,7 @@ import io.github.chrislo27.toolboks.version.Version
 
 object RHRE3 {
 
-    val VERSION: Version = Version(3, 0, 0, "")
+    val VERSION: Version = Version(3, 0, 1, "DEVELOPMENT")
     const val WIDTH = 1280
     const val HEIGHT = 720
     val DEFAULT_SIZE = WIDTH to HEIGHT
