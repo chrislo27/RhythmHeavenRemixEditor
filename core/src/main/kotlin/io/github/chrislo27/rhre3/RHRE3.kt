e2b01a7f24c244a1913521fe72289935a828ef96
chrislo27 <kinchrislo@gmail.com>
2017-09-01 09:50:32 -0700

    DEV version 3.1.0

--- core/src/main/kotlin/io/github/chrislo27/rhre3/RHRE3.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/RHRE3.kt
@@ -7,7 +7,7 @@ import io.github.chrislo27.toolboks.version.Version
 
 object RHRE3 {
 
-    val VERSION: Version = Version(3, 0, 5, "DEV")
+    val VERSION: Version = Version(3, 1, 0, "DEV")
     const val WIDTH = 1280
     const val HEIGHT = 720
     val DEFAULT_SIZE = WIDTH to HEIGHT
