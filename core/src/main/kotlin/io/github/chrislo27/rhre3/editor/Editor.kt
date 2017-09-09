ab9f4cc4470226e53dba449470d339f263f75084
chrislo27 <kinchrislo@gmail.com>
2017-09-08 20:22:43 -0700

    Split Editor.kt into two files for faster compilation

--- core/src/main/kotlin/io/github/chrislo27/rhre3/editor/Editor.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/editor/Editor.kt
@@ -26,16 +26,14 @@ import io.github.chrislo27.rhre3.entity.model.MultipartEntity
 import io.github.chrislo27.rhre3.entity.model.cue.CueEntity
 import io.github.chrislo27.rhre3.entity.model.multipart.EquidistantEntity
 import io.github.chrislo27.rhre3.entity.model.multipart.KeepTheBeatEntity
-import io.github.chrislo27.rhre3.entity.model.special.EndEntity
 import io.github.chrislo27.rhre3.entity.model.special.SubtitleEntity
 import io.github.chrislo27.rhre3.oopsies.GroupedAction
-import io.github.chrislo27.rhre3.oopsies.ReversibleAction
 import io.github.chrislo27.rhre3.registry.Game
 import io.github.chrislo27.rhre3.registry.GameRegistry
 import io.github.chrislo27.rhre3.registry.datamodel.ResponseModel
 import io.github.chrislo27.rhre3.registry.datamodel.impl.Cue
 import io.github.chrislo27.rhre3.screen.InfoScreen
-import io.github.chrislo27.rhre3.theme.DarkTheme
+import io.github.chrislo27.rhre3.theme.LightTheme
 import io.github.chrislo27.rhre3.theme.Theme
 import io.github.chrislo27.rhre3.track.PlayState
 import io.github.chrislo27.rhre3.track.Remix
@@ -123,7 +121,7 @@ class Editor(val main: RHRE3Application, stageCamera: OrthographicCamera)
 
             field.entities.forEach { it.updateInterpolation(true) }
         }
-    var theme: Theme = DarkTheme()
+    var theme: Theme = LightTheme()
     val stage: EditorStage = EditorStage(
             null, stageCamera, main, this)
     val batch: SpriteBatch
@@ -161,185 +159,6 @@ class Editor(val main: RHRE3Application, stageCamera: OrthographicCamera)
 
     fun prepAutosaveFile(baseFile: FileHandle) {
         autosaveFile = baseFile.sibling(baseFile.nameWithoutExtension() + ".autosave.${RHRE3.REMIX_FILE_EXTENSION}")
-    }
-
-    sealed class ClickOccupation {
-
-        interface TrackerBased {
-            var finished: Boolean
-            var final: Float
-        }
-
-        object None : ClickOccupation()
-
-        class Playback(val editor: Editor)
-            : ClickOccupation(), ReversibleAction<Remix>, TrackerBased {
-            val old = editor.remix.playbackStart
-            override var finished: Boolean = false
-            override var final: Float = Float.NaN
-                set(value) {
-                    if (!java.lang.Float.isNaN(field)) {
-                        error("Attempt to set value to $value when already set to $field")
-                    }
-                    field = value
-                }
-
-            override fun redo(context: Remix) {
-                if (final == Float.NaN)
-                    error("Final value was NaN which is impossible")
-                editor.remix.playbackStart = final
-            }
-
-            override fun undo(context: Remix) {
-                editor.remix.playbackStart = old
-            }
-        }
-
-        class Music(val editor: Editor, val middleClick: Boolean)
-            : ClickOccupation(), ReversibleAction<Remix>, TrackerBased {
-            val old = editor.remix.musicStartSec
-            override var finished: Boolean = false
-            override var final: Float = Float.NaN
-                set(value) {
-                    if (!java.lang.Float.isNaN(field)) {
-                        error("Attempt to set value to $value when already set to $field")
-                    }
-                    field = value
-                }
-
-            override fun redo(context: Remix) {
-                if (final != final)
-                    error("Final value was NaN which is impossible")
-                editor.remix.musicStartSec = final
-            }
-
-            override fun undo(context: Remix) {
-                editor.remix.musicStartSec = old
-            }
-        }
-
-        class CreatingSelection(val editor: Editor,
-                                val startPoint: Vector2) : ClickOccupation() {
-            val oldSelection = editor.selection.toList()
-            val rectangle = Rectangle()
-
-            fun updateRectangle() {
-                val startX = startPoint.x
-                val startY = startPoint.y
-                val width = editor.remix.camera.getInputX() - startX
-                val height = editor.remix.camera.getInputY() - startY
-
-                if (width < 0) {
-                    val abs = Math.abs(width)
-                    rectangle.x = startX - abs
-                    rectangle.width = abs
-                } else {
-                    rectangle.x = startX
-                    rectangle.width = width
-                }
-
-                if (height < 0) {
-                    val abs = Math.abs(height)
-                    rectangle.y = startY - abs
-                    rectangle.height = abs
-                } else {
-                    rectangle.y = startY
-                    rectangle.height = height
-                }
-            }
-
-        }
-
-        class SelectionDrag(val editor: Editor,
-                            val mouseOffset: Vector2,
-                            val isNewOrCopy: Boolean,
-                            val previousSelection: List<Entity>,
-                            val stretchType: StretchRegion)
-            : ClickOccupation() {
-
-            val oldBounds: List<Rectangle> = copyBounds(editor.selection)
-
-            val isStretching: Boolean by lazy { !isNewOrCopy && stretchType != StretchRegion.NONE }
-
-            private val selection: List<Entity>
-                get() = editor.selection
-
-            companion object {
-                fun copyBounds(selection: List<Entity>): List<Rectangle> =
-                        selection.map { Rectangle(it.bounds) }
-            }
-
-            val left: Float
-                get() = selection.minBy { it.bounds.x }?.bounds?.x ?: error("Nothing in selection")
-
-            val right: Float
-                get() {
-                    val right = selection.maxBy { it.bounds.x + it.bounds.width } ?: error("Nothing in selection")
-                    return right.bounds.x + right.bounds.width
-                }
-
-            val top: Float
-                get() {
-                    val highest = selection.maxBy { it.bounds.y } ?: error("Nothing in selection")
-                    return highest.bounds.y + highest.bounds.height
-                }
-
-            val bottom: Float
-                get() = selection.minBy { it.bounds.y }?.bounds?.y ?: error("Nothing in selection")
-
-            val width: Float by lazy {
-                right - left
-            }
-
-            val height: Int by lazy {
-                Math.round(top - bottom)
-            }
-
-            fun setFirstPosition(x: Float, y: Float) {
-                // reducing object creation due to rapid calling
-                val first = selection.first()
-                val oldFirstPosX = first.bounds.x
-                val oldFirstPosY = first.bounds.y
-                first.updateBounds {
-                    first.bounds.setPosition(x, y)
-                }
-
-                selection.forEachIndexed { index, entity ->
-                    if (index == 0)
-                        return@forEachIndexed
-                    entity.updateBounds {
-                        entity.bounds.x = (entity.bounds.x - oldFirstPosX) + x
-                        entity.bounds.y = (entity.bounds.y - oldFirstPosY) + y
-                    }
-                }
-            }
-
-            fun setPositionRelativeToMouse(snap: Float = editor.snap, intY: Boolean = true) {
-                val y = editor.remix.camera.getInputY() - mouseOffset.y
-                setFirstPosition(MathHelper.snapToNearest(editor.remix.camera.getInputX() - mouseOffset.x, snap),
-                                 if (intY) Math.round(y).toFloat() else y)
-            }
-
-            fun isPlacementValid(): Boolean {
-                if (isInDeleteZone())
-                    return false
-                if (top > TRACK_COUNT)
-                    return false
-
-                // EXCEPTIONS for special entities
-                if (selection.any { it is EndEntity } && editor.remix.entities.filter { it is EndEntity }.size > 1)
-                    return false
-
-                return editor.remix.entities.all {
-                    it in selection || selection.all { sel ->
-                        !sel.bounds.overlaps(it.bounds)
-                    }
-                }
-            }
-
-            fun isInDeleteZone(): Boolean = bottom < -0.5f
-
-        }
     }
 
     var clickOccupation: ClickOccupation = ClickOccupation.None
