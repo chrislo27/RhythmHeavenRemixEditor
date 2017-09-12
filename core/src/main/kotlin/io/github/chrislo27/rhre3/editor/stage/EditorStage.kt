193a12e05bd86634bd6e5055b64db2e1e0c3882e
chrislo27 <kinchrislo@gmail.com>
2017-09-12 06:51:14 -0700

    Tapalong button now extends all the way

--- core/src/main/kotlin/io/github/chrislo27/rhre3/editor/stage/EditorStage.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/editor/stage/EditorStage.kt
@@ -997,8 +997,8 @@ class EditorStage(parent: UIElement<EditorScreen>?,
             }
             buttonBarStage.elements += TapalongToggleButton(editor, this@EditorStage, palette, buttonBarStage,
                                                             buttonBarStage).apply {
-                this.location.set(screenWidth = size * 8,
-                                  screenX = size * 8 + padding * 8)
+                this.location.set(screenX = size * 8 + padding * 8)
+                this.location.set(screenWidth = pauseButton.location.screenX - this.location.screenX - padding)
             }
 
             // right aligned
