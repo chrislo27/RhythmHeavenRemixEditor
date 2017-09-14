4a189503f8f3bdbb0c97e58af6142b9461034187
chrislo27 <kinchrislo@gmail.com>
2017-09-13 19:38:01 -0700

    Add tint while views are active

--- core/src/main/kotlin/io/github/chrislo27/rhre3/editor/stage/ViewButton.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/editor/stage/ViewButton.kt
@@ -1,12 +1,13 @@
 package io.github.chrislo27.rhre3.editor.stage
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import io.github.chrislo27.rhre3.editor.Editor
 import io.github.chrislo27.rhre3.screen.EditorScreen
 import io.github.chrislo27.toolboks.i18n.Localization
-import io.github.chrislo27.toolboks.ui.Button
-import io.github.chrislo27.toolboks.ui.Stage
-import io.github.chrislo27.toolboks.ui.UIElement
-import io.github.chrislo27.toolboks.ui.UIPalette
+import io.github.chrislo27.toolboks.ui.*
+import io.github.chrislo27.toolboks.util.MathHelper
+import io.github.chrislo27.toolboks.util.gdxutils.setHSB
 
 
 class ViewButton(val editor: Editor, palette: UIPalette, parent: UIElement<EditorScreen>,
@@ -17,6 +18,21 @@ class ViewButton(val editor: Editor, palette: UIPalette, parent: UIElement<Edito
         return Localization["editor.view", if (editor.views.isEmpty())
             Localization["editor.view.default"]
         else editor.views.joinToString(separator = ", ") { Localization[it.localizationKey] }]
+    }
+
+    override fun render(screen: EditorScreen, batch: SpriteBatch, shapeRenderer: ShapeRenderer) {
+        if (labels.isNotEmpty()) {
+            val first = labels.first()
+            if (first is ImageLabel) {
+                if (editor.views.isNotEmpty()) {
+                    first.tint.setHSB(MathHelper.getSawtoothWave(1.5f), 0.3f, 0.75f)
+                } else {
+                    first.tint.set(1f, 1f, 1f, 1f)
+                }
+            }
+        }
+
+        super.render(screen, batch, shapeRenderer)
     }
 
     override fun onLeftClick(xPercent: Float, yPercent: Float) {
