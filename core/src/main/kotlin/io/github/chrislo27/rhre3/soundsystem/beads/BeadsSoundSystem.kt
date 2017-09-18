1e9a855aeb79d96f4fe9f7e4072f4ad07f65b39b
chrislo27 <kinchrislo@gmail.com>
2017-09-17 20:38:34 -0700

    Switch to using a temp file

--- core/src/main/kotlin/io/github/chrislo27/rhre3/soundsystem/beads/BeadsSoundSystem.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/soundsystem/beads/BeadsSoundSystem.kt
@@ -5,12 +5,16 @@ import com.badlogic.gdx.audio.Music
 import com.badlogic.gdx.audio.Sound
 import com.badlogic.gdx.backends.lwjgl.audio.OpenALMusic
 import com.badlogic.gdx.files.FileHandle
+import com.badlogic.gdx.utils.StreamUtils
 import io.github.chrislo27.rhre3.soundsystem.SoundSystem
 import io.github.chrislo27.toolboks.lazysound.LazySound
 import io.github.chrislo27.toolboks.util.FastSeekingMusic
 import net.beadsproject.beads.core.AudioContext
 import net.beadsproject.beads.core.AudioUtils
 import net.beadsproject.beads.core.io.JavaSoundAudioIO
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 object BeadsSoundSystem : SoundSystem() {
 
@@ -34,40 +38,22 @@ object BeadsSoundSystem : SoundSystem() {
         sampleData = run {
             val BUFFER_SIZE = 4096 * 4
             val audioBytes = ByteArray(BUFFER_SIZE)
-            var sampleBufferSize = BUFFER_SIZE
-            var data = ByteArray(sampleBufferSize)
-            var bytesRead: Int = 0
-            var totalBytesRead = 0
-
-            fun readAndSetBytesRead(): Int {
-                bytesRead = music.read(audioBytes)
-                return bytesRead
+            val tempFile = File.createTempFile("rhre3-data-${System.currentTimeMillis()}", "tmp").apply {
+                deleteOnExit()
             }
+            val fileOutStream = FileOutputStream(tempFile)
 
-            // this is just an expandable byte array, more memory concious than ByteArrayOutputStream
-            while (readAndSetBytesRead() > 0) {
-                // resize buf if necessary
-                if (bytesRead > sampleBufferSize - totalBytesRead) {
-                    sampleBufferSize = Math.max(sampleBufferSize * 2, sampleBufferSize + bytesRead)
-                    val newBuf = ByteArray(sampleBufferSize)
-                    System.arraycopy(data, 0, newBuf, 0, data.size)
-                    data = newBuf
-                }
-                System.arraycopy(audioBytes, 0, data, totalBytesRead, bytesRead)
-                totalBytesRead += bytesRead
+            while (true) {
+                val length = music.read(audioBytes)
+                if (length <= 0)
+                    break
+
+                fileOutStream.write(audioBytes, 0, length)
             }
+            StreamUtils.closeQuietly(fileOutStream)
 
-            System.gc()
-
-            // resize buf to proper length if necessary (trim)
-            if (sampleBufferSize > totalBytesRead) {
-                sampleBufferSize = totalBytesRead
-                val newBuf = ByteArray(sampleBufferSize)
-                System.arraycopy(data, 0, newBuf, 0, sampleBufferSize)
-                data = newBuf
-                System.gc()
-            }
-            val nFrames = sampleBufferSize / (2 * music.channels)
+            val data = tempFile.readBytes()
+            val nFrames = data.size / (2 * music.channels)
 
             // Copy and de-interleave entire data
             val sampleData = Array(music.channels) { FloatArray(nFrames) }
@@ -76,10 +62,17 @@ object BeadsSoundSystem : SoundSystem() {
             System.gc()
             AudioUtils.deinterleave(interleaved, music.channels, nFrames, sampleData)
 
+            try {
+                tempFile.delete()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
             sampleData
         }
 
         music.dispose()
+
 
         val sample = beadsAudio.sample
         sample.resize(sampleData[0].size.toLong())
