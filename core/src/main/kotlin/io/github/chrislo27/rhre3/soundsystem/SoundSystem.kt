7fef80c0773bde2eb77bbb8c7bf8502b0cb7c60d
chrislo27 <kinchrislo@gmail.com>
2017-09-17 17:17:42 -0700

    Add TODO Beads SS + flag for error on repeat system set

--- core/src/main/kotlin/io/github/chrislo27/rhre3/soundsystem/SoundSystem.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/soundsystem/SoundSystem.kt
@@ -4,11 +4,15 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.audio.Music
 import com.badlogic.gdx.audio.Sound
 import com.badlogic.gdx.files.FileHandle
+import io.github.chrislo27.toolboks.lazysound.LazySound
+import io.github.chrislo27.toolboks.util.FastSeekingMusic
 
 
 sealed class SoundSystem {
 
     companion object {
+
+        var errorOnDuplicateSystemSet = true
 
         lateinit var system: SoundSystem
             private set
@@ -16,9 +20,12 @@ sealed class SoundSystem {
         fun setSoundSystem(system: SoundSystem) {
             try {
                 SoundSystem.system
-                error("Sound system already set to ${system::class.simpleName}")
+
+                if (errorOnDuplicateSystemSet)
+                    error("Sound system already set to ${system::class.simpleName}")
             } catch (e: UninitializedPropertyAccessException) {
                 SoundSystem.system = system
+                system.onSet()
             }
         }
 
@@ -27,6 +34,10 @@ sealed class SoundSystem {
     abstract fun newSound(handle: FileHandle): Sound
 
     abstract fun newMusic(handle: FileHandle): Music
+
+    open fun onSet() {
+
+    }
 
 }
 
@@ -40,4 +51,21 @@ object GdxSoundSystem : SoundSystem() {
         return Gdx.audio.newMusic(handle)
     }
 
+}
+
+object BeadsSoundSystem : SoundSystem() {
+    override fun newSound(handle: FileHandle): Sound {
+        TODO()
+    }
+
+    override fun newMusic(handle: FileHandle): Music {
+        TODO()
+    }
+
+    override fun onSet() {
+        super.onSet()
+        // TODO
+        LazySound.soundFactory
+        FastSeekingMusic.musicFactory
+    }
 }
