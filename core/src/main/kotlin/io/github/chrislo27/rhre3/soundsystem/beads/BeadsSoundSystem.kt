d8109a3538141c931c58ef3d512a9b62248d3c8f
chrislo27 <kinchrislo@gmail.com>
2017-09-18 19:18:43 -0700

    Minimize memory footprint for reading music

--- core/src/main/kotlin/io/github/chrislo27/rhre3/soundsystem/beads/BeadsSoundSystem.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/soundsystem/beads/BeadsSoundSystem.kt
@@ -3,16 +3,12 @@ package io.github.chrislo27.rhre3.soundsystem.beads
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.backends.lwjgl.audio.OpenALMusic
 import com.badlogic.gdx.files.FileHandle
-import com.badlogic.gdx.utils.StreamUtils
 import io.github.chrislo27.rhre3.soundsystem.Music
 import io.github.chrislo27.rhre3.soundsystem.Sound
 import io.github.chrislo27.rhre3.soundsystem.SoundSystem
 import net.beadsproject.beads.core.AudioContext
 import net.beadsproject.beads.core.AudioUtils
 import net.beadsproject.beads.core.io.JavaSoundAudioIO
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
 import java.util.concurrent.CopyOnWriteArrayList
 
 object BeadsSoundSystem : SoundSystem() {
@@ -44,53 +40,51 @@ object BeadsSoundSystem : SoundSystem() {
     fun newAudio(handle: FileHandle): BeadsAudio {
         val music = Gdx.audio.newMusic(handle) as OpenALMusic
         val beadsAudio = BeadsAudio(music.channels, music.rate)
-        val sampleData: Array<FloatArray>
 
         music.reset()
 
-        sampleData = run {
+        run {
             val BUFFER_SIZE = 4096 * 4
             val audioBytes = ByteArray(BUFFER_SIZE)
-            val tempFile = File.createTempFile("rhre3-data-${System.currentTimeMillis()}", "tmp").apply {
-                deleteOnExit()
-            }
-            val fileOutStream = FileOutputStream(tempFile)
+            val sample = beadsAudio.sample
+            var length = 0L
 
             while (true) {
-                val length = music.read(audioBytes)
-                if (length <= 0)
+                val len = music.read(audioBytes)
+                if (len <= 0)
                     break
 
-                fileOutStream.write(audioBytes, 0, length)
+                length += len
             }
-            StreamUtils.closeQuietly(fileOutStream)
+            music.reset()
 
-            val data = tempFile.readBytes()
-            val nFrames = data.size / (2 * music.channels)
+            if (length >= Int.MAX_VALUE)
+                throw OutOfMemoryError("File too big")
 
-            // Copied from Beads JavaSoundAudioFile source code
-            // Copy and de-interleave entire data
-            val sampleData = Array(music.channels) { FloatArray(nFrames) }
-            val interleaved = FloatArray((music.channels * nFrames))
-            AudioUtils.byteToFloat(interleaved, data, false)
-            System.gc()
-            AudioUtils.deinterleave(interleaved, music.channels, nFrames, sampleData)
+            val nFrames = length / (2 * music.channels)
+            sample.resize(nFrames)
+            val interleaved = FloatArray(music.channels * (BUFFER_SIZE / (2 * music.channels)))
+            val sampleData = Array(music.channels) { FloatArray(interleaved.size) }
 
-            try {
-                tempFile.delete()
-            } catch (e: IOException) {
-                e.printStackTrace()
+            var currentFrame = 0
+            while (true) {
+                val len = music.read(audioBytes)
+                if (len <= 0)
+                    break
+
+                val framesOfDataRead = len / (2 * music.channels)
+
+                AudioUtils.byteToFloat(interleaved, audioBytes, false, len / 2) // 2 bytes per 16 bit float
+                AudioUtils.deinterleave(interleaved, music.channels, framesOfDataRead, sampleData)
+
+                sample.putFrames(currentFrame, sampleData, 0, framesOfDataRead)
+
+                currentFrame += framesOfDataRead
             }
-
-            sampleData
         }
 
         music.dispose()
 
-
-        val sample = beadsAudio.sample
-        sample.resize(sampleData[0].size.toLong())
-        sample.putFrames(0, sampleData)
 
         return beadsAudio
     }
