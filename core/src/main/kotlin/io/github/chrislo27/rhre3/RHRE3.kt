0e499291c69c8e73d97738a385c834d685446b9d
chrislo27 <kinchrislo@gmail.com>
2017-09-26 20:48:28 -0700

    Update DEV version again

--- core/src/main/kotlin/io/github/chrislo27/rhre3/RHRE3.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/RHRE3.kt
@@ -8,7 +8,7 @@ import io.github.chrislo27.toolboks.version.Version
 object RHRE3 {
 
     const val TITLE = "Rhythm Heaven Remix Editor 3"
-    val VERSION: Version = Version(3, 3, 1, "DEVELOPMENT")
+    val VERSION: Version = Version(3, 4, 0, "DEVELOPMENT")
     const val WIDTH = 1280
     const val HEIGHT = 720
     val DEFAULT_SIZE = WIDTH to HEIGHT
