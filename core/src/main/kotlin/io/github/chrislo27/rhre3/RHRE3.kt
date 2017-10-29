fc398652e2cc8607184c41ef96acd12c4e2dad02
chrislo27 <kinchrislo@gmail.com>
2017-10-29 14:05:48 -0700

    Make database branch unconstant

--- core/src/main/kotlin/io/github/chrislo27/rhre3/RHRE3.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/RHRE3.kt
@@ -8,7 +8,7 @@ import io.github.chrislo27.toolboks.version.Version
 object RHRE3 {
 
     const val TITLE = "Rhythm Heaven Remix Editor 3"
-    val VERSION: Version = Version(3, 5, 0, "DEVELOPMENT")
+    val VERSION: Version = Version(3, 5, 0, "RC1")
     const val WIDTH = 1280
     const val HEIGHT = 720
     val DEFAULT_SIZE = WIDTH to HEIGHT
@@ -25,10 +25,10 @@ object RHRE3 {
     const val GITHUB: String = "https://github.com/chrislo27/RhythmHeavenRemixEditor"
     const val GITHUB_RELEASES = "$GITHUB/releases"
     const val DATABASE_URL: String = "https://github.com/chrislo27/RHRE-database.git"
-    const val DEV_DATABASE_BRANCH: String = "dev"
-    const val MASTER_DATABASE_BRANCH: String = "master"
-    const val DATABASE_BRANCH: String = DEV_DATABASE_BRANCH
-    const val DATABASE_CURRENT_VERSION: String = "https://raw.githubusercontent.com/chrislo27/RHRE-database/$DATABASE_BRANCH/current.json"
+    private const val DEV_DATABASE_BRANCH: String = "dev"
+    private const val MASTER_DATABASE_BRANCH: String = "master"
+    val DATABASE_BRANCH: String = DEV_DATABASE_BRANCH // CANNOT be const
+    val DATABASE_CURRENT_VERSION: String = "https://raw.githubusercontent.com/chrislo27/RHRE-database/$DATABASE_BRANCH/current.json"
     const val DATABASE_RELEASES = "https://github.com/chrislo27/RHRE-database/releases"
     const val RELEASE_API_URL = "https://api.github.com/repos/chrislo27/RhythmHeavenRemixEditor/releases/latest"
     const val OUT_OF_MEMORY_DOC_LINK: String = "http://rhre.readthedocs.io/en/latest/Out-of-memory-on-music/"
