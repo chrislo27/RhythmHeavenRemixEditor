7cd2d2cf810ee448f32e8c7293a5111db0b06ac4
chrislo27 <kinchrislo@gmail.com>
2017-11-25 22:29:13 -0800

    Rename a class

--- core/src/main/kotlin/io/github/chrislo27/rhre3/soundsystem/beads/DaemonJavaSoundAudioIO.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/soundsystem/beads/DaemonJavaSoundAudioIO.kt
@@ -6,13 +6,13 @@ import kotlin.concurrent.thread
 /**
  * The default JavaSoundAudioIO thread is non-daemon. This class uses reflection to make it a daemon thread.
  */
-class ImprovedJavaSoundAudioIO : JavaSoundAudioIO() {
+class DaemonJavaSoundAudioIO : JavaSoundAudioIO() {
 
     override fun start(): Boolean {
         fun runRealTime() {
             JavaSoundAudioIO::class.java.getDeclaredMethod("runRealTime").apply {
                 isAccessible = true
-                invoke(this@ImprovedJavaSoundAudioIO)
+                invoke(this@DaemonJavaSoundAudioIO)
             }
         }
 
@@ -26,7 +26,7 @@ class ImprovedJavaSoundAudioIO : JavaSoundAudioIO() {
 
         JavaSoundAudioIO::class.java.getDeclaredField("audioThread").apply {
             isAccessible = true
-            set(this@ImprovedJavaSoundAudioIO, audioThread)
+            set(this@DaemonJavaSoundAudioIO, audioThread)
         }
 
         return true
