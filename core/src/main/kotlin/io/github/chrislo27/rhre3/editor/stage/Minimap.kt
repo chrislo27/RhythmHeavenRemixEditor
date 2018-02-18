4897f0375b121744feca287e2b34f65c04c121c0
chrislo27 <kinchrislo@gmail.com>
2018-02-17 17:25:36 -0800

    Add border to minimap preview + hide when scrolling camera

--- core/src/main/kotlin/io/github/chrislo27/rhre3/editor/stage/Minimap.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/editor/stage/Minimap.kt
@@ -56,63 +56,65 @@ class Minimap(val editor: Editor, palette: UIPalette, parent: UIElement<EditorSc
             rect(location.realX - 1, location.realY, location.realWidth + 1, location.realHeight)
             end()
         }.useStencilMask {
-            val x = location.realX
-            val y = location.realY
-            val pxHeight = location.realHeight
-            val pxWidth = location.realWidth
-            val unitHeight = pxHeight / Editor.TRACK_COUNT
-            val unitWidth = pxWidth / maxX
+                    val x = location.realX
+                    val y = location.realY
+                    val pxHeight = location.realHeight
+                    val pxWidth = location.realWidth
+                    val unitHeight = pxHeight / Editor.TRACK_COUNT
+                    val unitWidth = pxWidth / maxX
 
-            // entities
-            remix.entities.forEach { entity ->
-                if (entity !is ModelEntity<*>)
-                    return@forEach
+                    // entities
+                    remix.entities.forEach { entity ->
+                        if (entity !is ModelEntity<*>)
+                            return@forEach
 
-                batch.color = entity.getRenderColor()
-                batch.fillRect(x + entity.bounds.x * unitWidth, y + entity.bounds.y * unitHeight,
-                               entity.bounds.width * unitWidth, unitHeight)
-            }
+                        batch.color = entity.getRenderColor()
+                        batch.fillRect(x + entity.bounds.x * unitWidth, y + entity.bounds.y * unitHeight,
+                                       entity.bounds.width * unitWidth, unitHeight)
+                    }
 
-            // horizontal lines
-            batch.color = editor.theme.trackLine
-            for (i in 1..Editor.TRACK_COUNT) {
-                batch.fillRect(x, y + i * unitHeight - 0.5f, pxWidth, 1f)
-            }
-            batch.fillRect(x - 0.5f, y, 1f, pxHeight)
-            batch.fillRect(x + pxWidth - 0.5f, y, 1f, pxHeight)
-            batch.setColor(1f, 1f, 1f, 1f)
+                    // horizontal lines
+                    batch.color = editor.theme.trackLine
+                    for (i in 1..Editor.TRACK_COUNT) {
+                        batch.fillRect(x, y + i * unitHeight - 0.5f, pxWidth, 1f)
+                    }
+                    batch.fillRect(x - 0.5f, y, 1f, pxHeight)
+                    batch.fillRect(x + pxWidth - 0.5f, y, 1f, pxHeight)
+                    batch.setColor(1f, 1f, 1f, 1f)
 
-            // trackers
-            editor.remix.trackers.forEach { tc ->
-                tc.map.values.forEach { tracker ->
-                    batch.color = tracker.getColour(editor.theme)
-                    batch.fillRect(x + tracker.beat * unitWidth - 0.5f, y, 1f, pxHeight)
+                    // trackers
+                    editor.remix.trackers.forEach { tc ->
+                        tc.map.values.forEach { tracker ->
+                            batch.color = tracker.getColour(editor.theme)
+                            batch.fillRect(x + tracker.beat * unitWidth - 0.5f, y, 1f, pxHeight)
+                        }
+                    }
+                    run {
+                        batch.color = editor.theme.trackers.musicStart
+                        batch.fillRect(x + remix.tempos.secondsToBeats(remix.musicStartSec) * unitWidth - 0.5f, y, 1f,
+                                       pxHeight)
+                        val playback = if (remix.playState == PlayState.STOPPED) remix.playbackStart else remix.beat
+                        batch.color = editor.theme.trackers.playback
+                        batch.fillRect(x + playback * unitWidth - 0.5f, y, 1f, pxHeight)
+                    }
+
+                    // camera box
+                    batch.setColor(1f, 1f, 1f, 1f)
+                    val camera = editor.camera
+                    batch.drawRect(x + (camera.position.x - camera.viewportWidth / 2) * unitWidth, y,
+                                   camera.viewportWidth * unitWidth, pxHeight, 2f)
+
+                    if (isMouseOver() && editor.clickOccupation == ClickOccupation.None
+                            && remix.playState == PlayState.STOPPED
+                            && Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+                        val percent = (stage.camera.getInputX() - location.realX) / location.realWidth
+                        editor.camera.position.x = percent * maxX
+                    }
                 }
-            }
-            run {
-                batch.color = editor.theme.trackers.musicStart
-                batch.fillRect(x + remix.tempos.secondsToBeats(remix.musicStartSec) * unitWidth - 0.5f, y, 1f, pxHeight)
-                val playback = if (remix.playState == PlayState.STOPPED) remix.playbackStart else remix.beat
-                batch.color = editor.theme.trackers.playback
-                batch.fillRect(x + playback * unitWidth - 0.5f, y, 1f, pxHeight)
-            }
-
-            // camera box
-            batch.setColor(1f, 1f, 1f, 1f)
-            val camera = editor.camera
-            batch.drawRect(x + (camera.position.x - camera.viewportWidth / 2) * unitWidth, y,
-                           camera.viewportWidth * unitWidth, pxHeight, 2f)
-
-            if (isMouseOver() && editor.clickOccupation == ClickOccupation.None
-                    && remix.playState == PlayState.STOPPED
-                    && Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-                val percent = (stage.camera.getInputX() - location.realX) / location.realWidth
-                editor.camera.position.x = percent * maxX
-            }
-        }
 
         // Preview
-        if (timeHovered >= 1f && bufferSupported && editor.main.preferences.getBoolean(PreferenceKeys.SETTINGS_MINIMAP_PREVIEW, true)) {
+        if (timeHovered >= 1f && bufferSupported && !Gdx.input.isButtonPressed(Input.Buttons.LEFT)
+                && editor.main.preferences.getBoolean(PreferenceKeys.SETTINGS_MINIMAP_PREVIEW, true)) {
             val percent = (stage.camera.getInputX() - location.realX) / location.realWidth
             val centreX = percent * maxX
             val oldCamX = editor.camera.position.x
@@ -135,9 +137,13 @@ class Minimap(val editor: Editor, palette: UIPalette, parent: UIElement<EditorSc
 
             val bufSecH = (buffer.height * 0.75f).roundToInt()
             val h = location.realWidth * (bufSecH.toFloat() / buffer.width)
+            val outline = 2f
             batch.color = editor.theme.background
             batch.fillRect(location.realX, location.realY + location.realHeight,
                            location.realWidth, h)
+            batch.setColor(editor.theme.trackLine.r, editor.theme.trackLine.g, editor.theme.trackLine.b, 1f)
+            batch.drawRect(location.realX - outline, location.realY + location.realHeight,
+                           location.realWidth + outline * 2, h + outline, outline)
             batch.setColor(1f, 1f, 1f, 1f)
             batch.draw(buffer.colorBufferTexture,
                        location.realX, location.realY + location.realHeight,
@@ -150,7 +156,8 @@ class Minimap(val editor: Editor, palette: UIPalette, parent: UIElement<EditorSc
         super.frameUpdate(screen)
         this.visible = !screen.main.preferences.getBoolean(PreferenceKeys.SETTINGS_MINIMAP, false)
         if (isMouseOver()) {
-            timeHovered += Gdx.graphics.deltaTime
+            if (!Gdx.input.isButtonPressed(Input.Buttons.LEFT))
+                timeHovered += Gdx.graphics.deltaTime
         } else {
             timeHovered = 0f
         }
