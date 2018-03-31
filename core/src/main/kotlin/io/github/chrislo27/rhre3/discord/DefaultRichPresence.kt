38726ebb38ac47be98fa74b3c527814960d342ae
chrislo27 <kinchrislo@gmail.com>
2018-03-30 23:09:41 -0700

    Add various small icons for presence states

--- core/src/main/kotlin/io/github/chrislo27/rhre3/discord/DefaultRichPresence.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/discord/DefaultRichPresence.kt
@@ -5,13 +5,18 @@ import io.github.chrislo27.rhre3.RHRE3
 import io.github.chrislo27.rhre3.RHRE3Application
 
 
-class DefaultRichPresence(state: String = "", party: Pair<Int, Int> = DEFAULT_PARTY) : DiscordRichPresence() {
+class DefaultRichPresence(state: String = "",
+                          party: Pair<Int, Int> = DEFAULT_PARTY,
+                          smallIcon: String = "",
+                          smallIconText: String = state)
+    : DiscordRichPresence() {
 
     companion object {
         val DEFAULT_PARTY: Pair<Int, Int> = 0 to 0
     }
 
-    constructor(presenceState: PresenceState) : this(presenceState.state, presenceState.getPartyCount()) {
+    constructor(presenceState: PresenceState)
+            : this(presenceState.state, presenceState.getPartyCount(), presenceState.smallIcon, presenceState.smallIconText) {
         presenceState.modifyRichPresence(this)
     }
 
@@ -26,6 +31,8 @@ class DefaultRichPresence(state: String = "", party: Pair<Int, Int> = DEFAULT_PA
         startTimestamp = RHRE3Application.instance.startTimeMillis / 1000L // Epoch seconds
         largeImageKey = DiscordHelper.DEFAULT_LARGE_IMAGE
         largeImageText = RHRE3.GITHUB
+        smallImageKey = smallIcon
+        smallImageText = smallIconText
         this.state = state
         if (party.first > 0 && party.second > 0) {
             partySize = party.first
--- core/src/main/kotlin/io/github/chrislo27/rhre3/discord/PresenceState.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/discord/PresenceState.kt
@@ -3,7 +3,7 @@ package io.github.chrislo27.rhre3.discord
 import kotlin.math.roundToLong
 
 
-sealed class PresenceState(open val state: String = "") {
+sealed class PresenceState(open val state: String = "", open val smallIcon: String = "", open val smallIconText: String = state) {
 
     open fun getPartyCount(): Pair<Int, Int> = DefaultRichPresence.DEFAULT_PARTY
 
@@ -13,25 +13,25 @@ sealed class PresenceState(open val state: String = "") {
     // ---------------- IMPLEMENTATIONS BELOW ----------------
 
     object Loading
-        : PresenceState("Loading...")
+        : PresenceState("Loading...", "gear")
 
     object InEditor
         : PresenceState("In Editor")
 
     object Exporting
-        : PresenceState("Exporting a remix")
+        : PresenceState("Exporting a remix", "export")
 
     object Uploading
-        : PresenceState("Uploading a remix")
+        : PresenceState("Uploading a remix", "export")
 
     object InSettings
-        : PresenceState("In Info and Settings")
+        : PresenceState("In Info and Settings", "info")
 
     object ViewingCredits
-        : PresenceState("Viewing the credits ❤")
+        : PresenceState("Viewing the credits ❤", "credits")
 
-    sealed class Elapsable(state: String, val duration: Float)
-        : PresenceState(state) {
+    sealed class Elapsable(state: String, val duration: Float, smallIcon: String = "", smallIconText: String = state)
+        : PresenceState(state, smallIcon, smallIconText) {
         override fun modifyRichPresence(richPresence: DefaultRichPresence) {
             super.modifyRichPresence(richPresence)
             if (duration > 0f) {
@@ -43,7 +43,7 @@ sealed class PresenceState(open val state: String = "") {
             : Elapsable("Playing a MIDI", duration)
 
         class PresentationMode(duration: Float)
-            : Elapsable("In Presentation Mode", duration)
+            : Elapsable("In Presentation Mode", duration, "presentation_mode")
     }
 
 }
