7de64788557c7203dbc1a34a593b2429d39a9386
chrislo27 <kinchrislo@gmail.com>
2018-07-01 14:19:36 -0700

    Don't store actual bccad file

--- core/src/main/kotlin/io/github/chrislo27/rhre3/credits/CreditsGame.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/credits/CreditsGame.kt
@@ -27,6 +27,7 @@ import io.github.chrislo27.toolboks.util.gdxutils.scaleMul
 import rhmodding.bccadeditor.bccad.Animation
 import rhmodding.bccadeditor.bccad.BCCAD
 import rhmodding.bccadeditor.bccad.Sprite
+import java.util.*
 import kotlin.math.roundToInt
 
 
@@ -46,7 +47,7 @@ class CreditsGame(main: RHRE3Application) : ToolboksScreen<RHRE3Application, Cre
         setToOrtho(false, 240 / 9f * 16f, 240f)
     }
     private val music: Music by lazy { Gdx.audio.newMusic(GitHelper.SOUNDS_DIR.child("etc/jumpinjazzsfx.ogg")) }
-    private val bccad: BCCAD = BCCAD(Gdx.files.internal("credits/frog.bccad"))
+    private val bccad: BCCAD = BCCAD(Base64.getDecoder().decode(Gdx.files.internal("credits/frog.bin").readString("UTF-8")))
     private val sheet: Texture by lazy { AssetRegistry.get<Texture>("credits_frog") }
     private val bgTex: Texture by lazy { AssetRegistry.get<Texture>("credits_bg") }
     private val frameBuffer: FrameBuffer = FrameBuffer(Pixmap.Format.RGBA8888, camera.viewportWidth.roundToInt(), camera.viewportHeight.roundToInt(), false)
--- core/src/main/kotlin/rhmodding/bccadeditor/bccad/BCCAD.kt
+++ core/src/main/kotlin/rhmodding/bccadeditor/bccad/BCCAD.kt
@@ -4,53 +4,54 @@ import com.badlogic.gdx.files.FileHandle
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
-class BCCAD(file: FileHandle) {
-	var timestamp: Int
-	var sheetW: Short
-	var sheetH: Short
-	val sprites = mutableListOf<Sprite>()
-	val animations = mutableListOf<Animation>()
+class BCCAD {
+    var timestamp: Int
+    var sheetW: Short
+    var sheetH: Short
+    val sprites = mutableListOf<Sprite>()
+    val animations = mutableListOf<Animation>()
 
-	init {
-		val b = file.readBytes()
-		val buf = ByteBuffer.wrap(b)
-		buf.order(ByteOrder.LITTLE_ENDIAN)
-		buf.position(0)
-		timestamp = buf.int
-		sheetW = buf.short
-		sheetH = buf.short
-		repeat(buf.int) {
-			sprites.add(Sprite.fromBuffer(buf))
-		}
-		repeat(buf.int) {
-			animations.add(Animation.fromBuffer(buf))
-		}
-	}
+    constructor(b: ByteArray) {
+        val buf = ByteBuffer.wrap(b)
+        buf.order(ByteOrder.LITTLE_ENDIAN)
+        buf.position(0)
+        timestamp = buf.int
+        sheetW = buf.short
+        sheetH = buf.short
+        repeat(buf.int) {
+            sprites.add(Sprite.fromBuffer(buf))
+        }
+        repeat(buf.int) {
+            animations.add(Animation.fromBuffer(buf))
+        }
+    }
 
-	fun toBytes(): ByteArray {
-		val firstBytes = ByteArray(12)
-		val buf = ByteBuffer.wrap(firstBytes).order(ByteOrder.LITTLE_ENDIAN)
-		buf.putInt(timestamp)
-		buf.putShort(sheetW)
-		buf.putShort(sheetH)
-		buf.putInt(sprites.size)
-		val l = firstBytes.toMutableList()
-		for (s in sprites) {
-			l.addAll(s.toBytes())
-		}
-		val animationSizeBytes = ByteArray(4)
-		ByteBuffer.wrap(animationSizeBytes).order(ByteOrder.LITTLE_ENDIAN).putInt(animations.size)
-		l.addAll(animationSizeBytes.toList())
-		for (a in animations) {
-			l.addAll(a.toBytes())
-		}
-		l.add(0)
-		return l.toByteArray()
-	}
+    constructor(file: FileHandle) : this(file.readBytes())
 
-	override fun toString(): String {
-		return "$timestamp $sheetW $sheetH ${sprites.size} ${animations.size}\n" +
-				"Sprites: {\n" + sprites.map { "\t" + it.toString() }.joinToString("\n") + "\n}" +
-				"\nAnimations: {\n" + animations.map { "\t" + it.toString() }.joinToString("\n") + "\n}"
-	}
+    fun toBytes(): ByteArray {
+        val firstBytes = ByteArray(12)
+        val buf = ByteBuffer.wrap(firstBytes).order(ByteOrder.LITTLE_ENDIAN)
+        buf.putInt(timestamp)
+        buf.putShort(sheetW)
+        buf.putShort(sheetH)
+        buf.putInt(sprites.size)
+        val l = firstBytes.toMutableList()
+        for (s in sprites) {
+            l.addAll(s.toBytes())
+        }
+        val animationSizeBytes = ByteArray(4)
+        ByteBuffer.wrap(animationSizeBytes).order(ByteOrder.LITTLE_ENDIAN).putInt(animations.size)
+        l.addAll(animationSizeBytes.toList())
+        for (a in animations) {
+            l.addAll(a.toBytes())
+        }
+        l.add(0)
+        return l.toByteArray()
+    }
+
+    override fun toString(): String {
+        return "$timestamp $sheetW $sheetH ${sprites.size} ${animations.size}\n" +
+                "Sprites: {\n" + sprites.map { "\t" + it.toString() }.joinToString("\n") + "\n}" +
+                "\nAnimations: {\n" + animations.map { "\t" + it.toString() }.joinToString("\n") + "\n}"
+    }
 }
