4813768a10e062867afaf1c1ec0153f51513f037
chrislo27 <kinchrislo@gmail.com>
2018-07-01 22:53:20 -0700

    Improve animation of expansion splash screen significantly

--- core/src/main/kotlin/io/github/chrislo27/rhre3/screen/ExpansionSplashScreen.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/screen/ExpansionSplashScreen.kt
@@ -8,20 +8,21 @@ import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.math.Interpolation
+import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.utils.Align
 import io.github.chrislo27.rhre3.PreferenceKeys
 import io.github.chrislo27.rhre3.RHRE3
 import io.github.chrislo27.rhre3.RHRE3Application
 import io.github.chrislo27.toolboks.ToolboksScreen
 import io.github.chrislo27.toolboks.registry.AssetRegistry
-import io.github.chrislo27.toolboks.util.MathHelper
 import io.github.chrislo27.toolboks.util.gdxutils.drawCompressed
 import io.github.chrislo27.toolboks.util.gdxutils.drawQuad
 import io.github.chrislo27.toolboks.util.gdxutils.fillRect
+import kotlin.math.roundToInt
 
 
 class ExpansionSplashScreen(main: RHRE3Application, val nextScreen: Screen?)
-    : ToolboksScreen<RHRE3Application, ExpansionSplashScreen>(main) {
+    : ToolboksScreen<RHRE3Application, ExpansionSplashScreen>(main), HidesVersionText {
 
     private val camera = OrthographicCamera().apply {
         viewportWidth = RHRE3.WIDTH.toFloat()
@@ -36,8 +37,18 @@ class ExpansionSplashScreen(main: RHRE3Application, val nextScreen: Screen?)
     private val tempBottomColor: Color = Color(bottomColor)
     private var timeElapsed = 0f
     private val interpolation: Interpolation get() = Interpolation.pow5Out
-    private val logoAlpha get() = interpolation.apply((timeElapsed / 4).coerceAtMost(1f)).coerceIn(0f, 1f)
-    private val textAlpha get() = interpolation.apply(((timeElapsed - 2) / 2).coerceIn(0f, 1f)).coerceIn(0f, 1f)
+    private val gradientAlpha get() = Interpolation.pow2.apply(((timeElapsed) / 4).coerceAtMost(1f)).coerceIn(0f, 1f)
+    private val logoAlpha get() = interpolation.apply(((timeElapsed - 2) / 2).coerceAtMost(1f)).coerceIn(0f, 1f)
+    private val textAlpha get() = interpolation.apply(((timeElapsed - 4) / 2).coerceIn(0f, 1f))
+    private val enterAlpha get() = Interpolation.circleOut.apply(((timeElapsed - 5.5f) / 0.5f).coerceIn(0f, 1f))
+    private val colorAlpha get() = Interpolation.sine.apply(((timeElapsed - 6f) / 3f).coerceAtLeast(0f))
+
+    // Animation:
+    // Gradient begins to climb for first 2 seconds (to 50%)
+    // EXPANSION text ascends for 2 seconds
+    // Detailed text ascends in 2 seconds, logo scales down
+    // Gradient can begin fading in and out, complete period of 6 seconds
+    // ENTER text fades in in 0.5 seconds, @5.5 seconds
 
     private fun BitmapFont.scaleFont() {
         this.setUseIntegerPositions(false)
@@ -62,21 +73,22 @@ class ExpansionSplashScreen(main: RHRE3Application, val nextScreen: Screen?)
         batch.fillRect(0f, 0f, camera.viewportWidth, camera.viewportHeight)
         batch.setColor(1f, 1f, 1f, 1f)
 
-        val gradientHeight = camera.viewportHeight * 0.75f
-        val gradientY = gradientHeight * logoAlpha - gradientHeight
+        val gradientHeight = camera.viewportHeight * 0.85f
+        val gradientY = gradientHeight * gradientAlpha - gradientHeight
+        val logoY = gradientHeight * logoAlpha - gradientHeight
         // Glowing gradient
-        tempBottomColor.set(bottomColor).lerp(bottomColorGlow, MathHelper.getBaseCosineWave(3f))
+        tempBottomColor.set(bottomColor).lerp(bottomColorGlow, colorAlpha)
         val bottomGlow = tempBottomColor.toFloatBits()
         batch.drawQuad(0f, gradientY, bottomGlow, camera.viewportWidth, gradientY, bottomGlow,
                        camera.viewportWidth, gradientY + gradientHeight, topColor.toFloatBits(), 0f, gradientY + gradientHeight, topColor.toFloatBits())
 
         batch.setColor(1f, 1f, 1f, 1f)
         val logo = AssetRegistry.get<Texture>("logo_1024")
-        val logoSize = camera.viewportWidth * 0.3f
+        val logoSize = camera.viewportHeight * MathUtils.lerp(0.8f, 0.55f, textAlpha)
         val expansionSize = logoSize * 1.4f
         batch.draw(logo, camera.viewportWidth * 0.5f - logoSize / 2f, camera.viewportHeight - logoSize, logoSize, logoSize)
 
-        batch.draw(AssetRegistry.get<Texture>("logo_expansion_text"), camera.viewportWidth * 0.5f - expansionSize / 2f, gradientY * 2 + (camera.viewportHeight * 1.1f - expansionSize), expansionSize, expansionSize)
+        batch.draw(AssetRegistry.get<Texture>("logo_expansion_text"), camera.viewportWidth * 0.5f - expansionSize / 2f, logoY * 2 + (camera.viewportHeight * 1.1f - expansionSize), expansionSize, expansionSize)
 
         val font = main.defaultBorderedFont
         font.setColor(1f, 1f, 1f, 1f)
@@ -86,8 +98,10 @@ class ExpansionSplashScreen(main: RHRE3Application, val nextScreen: Screen?)
         val textY = gradientHeight * textAlpha - gradientHeight
         font.drawCompressed(batch, "Welcome to the RHRExpansion.", 0f, textY + camera.viewportHeight * 0.35f, camera.viewportWidth, Align.center)
         font.drawCompressed(batch, "➡ Resize the track\n➡ Store your own patterns\n➡ Use swing tempo\n[X]➡ []And more...", camera.viewportWidth * 0.35f, textY + camera.viewportHeight * 0.30f, camera.viewportWidth * 0.3f, Align.left)
-        font.drawCompressed(batch, "View the release changelog for all the details.", 0f, textY + camera.viewportHeight * 0.1125f, camera.viewportWidth, Align.center)
-        font.drawCompressed(batch, "[[[CYAN]ENTER[]]", 0f, textY + camera.viewportHeight * 0.05f, camera.viewportWidth, Align.center)
+        font.drawCompressed(batch, "View the release changelog for more details.", 0f, textY + camera.viewportHeight * 0.1125f, camera.viewportWidth, Align.center)
+
+        font.setColor(1f, 1f, 1f, enterAlpha)
+        font.drawCompressed(batch, "[[[#00FFFF${(enterAlpha * 255).roundToInt().toString(16).padStart(2, '0')}]ENTER[]]", 0f, textY + camera.viewportHeight * 0.05f, camera.viewportWidth, Align.center)
 
         font.setColor(1f, 1f, 1f, 1f)
         font.unscaleFont()
@@ -104,14 +118,14 @@ class ExpansionSplashScreen(main: RHRE3Application, val nextScreen: Screen?)
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.R)) {
             timeElapsed = 0f
-        } else if (textAlpha >= 1f && (Gdx.input.isKeyJustPressed(Input.Keys.ENTER) || Gdx.input.isKeyJustPressed(Input.Keys.SPACE))) {
+        } else if (enterAlpha >= 1f && (Gdx.input.isKeyJustPressed(Input.Keys.ENTER) || Gdx.input.isKeyJustPressed(Input.Keys.SPACE))) {
             main.preferences.putBoolean(PreferenceKeys.SEEN_EXPANSION_SPLASH, true).flush()
             main.screen = nextScreen
         }
     }
 
     override fun getDebugString(): String? {
-        return "time: $timeElapsed"
+        return "time: $timeElapsed\ngradient: $gradientAlpha\nlogo: $logoAlpha\ntext: $textAlpha\nenter: $enterAlpha\ncolor: $colorAlpha"
     }
 
     override fun tickUpdate() {
