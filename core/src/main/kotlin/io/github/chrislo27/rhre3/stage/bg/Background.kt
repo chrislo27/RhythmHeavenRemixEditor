9aae41464f0a7435cfe813a12a7635df608be656
chrislo27 <kinchrislo@gmail.com>
2018-07-07 10:49:23 -0700

    Remove boring RHDS bg

--- core/src/main/kotlin/io/github/chrislo27/rhre3/stage/bg/Background.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/stage/bg/Background.kt
@@ -15,8 +15,7 @@ abstract class Background(val id: String) {
                 TengokuBackground("tengoku"),
                 KarateManBackground("karateMan"),
                 TilingBackground("tiled", 5f, speedX = 1f, speedY = 1f) { AssetRegistry["bg_tile"] },
-                TilingBackground("rhdsPolkaDots", 5f, speedX = 3f, speedY = -3f, widthCoeff = 0.5f, heightCoeff = 0.5f) { AssetRegistry["bg_polkadot"] },
-                DSBackground("rhdsBoring")
+                TilingBackground("rhdsPolkaDots", 5f, speedX = 3f, speedY = -3f, widthCoeff = 0.5f, heightCoeff = 0.5f) { AssetRegistry["bg_polkadot"] }
                                                           ) }
         val backgroundMap: Map<String, Background> by lazy { backgrounds.associateBy(Background::id) + listOf("TENGOKU" to backgrounds.first { it.id == "tengoku" }, "KARATE_MAN" to backgrounds.first { it.id == "karateMan" }, "TILED" to backgrounds.first { it.id == "tiled" }) }
         val defaultBackground: Background
