8f75d1d0da0c68c9739dae730ef7025e8db18bb4
chrislo27 <kinchrislo@gmail.com>
2018-07-08 21:10:44 -0700

    Update credits

--- core/src/main/kotlin/io/github/chrislo27/rhre3/credits/Credits.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/credits/Credits.kt
@@ -23,7 +23,7 @@ object Credits {
             "specialThanks" crediting "baguette, GlitchyPSIX, Miracle22, Pengu123, TheRhythmKid, TheGarnet_, " +
                     "(◉.◉)☂, GrueKun, nerd, ChorusSquid, ArsenArsen, Clone5184, danthonywalker, jos, susmobile, " +
                     "Lvl100Feraligatr, SuicuneWiFi, Dracobot, AngryTapper, Zenon, inkedsplat, RobSetback, Mixelz, " +
-                    "iRonnoc5, sp00pster, RHModding Discord server",
+                    "iRonnoc5, sp00pster, Alchemyking, RHModding Discord server",
             "rhre2" crediting
                     "(◉.◉)☂, ahemtoday, Altonotone, ArendAlphaEagle, Armodillomatt12, baguette, Chef May, " +
                     "ChocolateJake, ChorusSquid, Chowder, David Mismo, Dragoneteur, Eggman199, " +
