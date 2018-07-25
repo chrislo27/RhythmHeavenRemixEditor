c96f2e22e534b11d428b90c0b64c65d87f400537
chrislo27 <kinchrislo@gmail.com>
2018-07-25 13:48:45 -0700

    Add bug icon

--- core/src/main/kotlin/io/github/chrislo27/rhre3/init/DefaultAssetLoader.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/init/DefaultAssetLoader.kt
@@ -99,6 +99,7 @@ class DefaultAssetLoader : AssetRegistry.IAssetLoader {
         AssetRegistry.loadAsset<Texture>("ui_icon_pencil", "images/ui/icons/pencil.png")
         AssetRegistry.loadAsset<Texture>("ui_icon_x", "images/ui/icons/x.png")
         AssetRegistry.loadAsset<Texture>("ui_icon_donate", "images/ui/donate.png")
+        AssetRegistry.loadAsset<Texture>("ui_icon_bug", "images/ui/icons/bug.png")
 
         AssetRegistry.loadAsset<Texture>("ui_search_clear", "images/ui/searchbar/clear.png")
         AssetRegistry.loadAsset<Texture>("ui_search_filter_gameName", "images/ui/searchbar/gameName.png")
