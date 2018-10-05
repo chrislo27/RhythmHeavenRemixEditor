797564b504b8c0e24e6e202121d9a4c08d8eecd7
chrislo27 <kinchrislo@gmail.com>
2018-10-04 20:22:00 -0700

    Fix pattern preview button appearing after scrolling during playback

--- core/src/main/kotlin/io/github/chrislo27/rhre3/editor/stage/EditorStage.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/editor/stage/EditorStage.kt
@@ -377,7 +377,7 @@ class EditorStage(parent: UIElement<EditorScreen>?,
                 editStoredPatternButton.visible = storedPatternsFilter.currentPattern != null
                 patternPreviewButton.visible = false
             } else {
-                patternPreviewButton.visible = true // Allows the update method to change visibility
+                patternPreviewButton.visible = editor.remix.playState == PlayState.STOPPED // Allows the update method to change visibility if stopped
                 patternPreviewButton.update(if (anyDatamodels) filter.currentDatamodel else null)
                 editStoredPatternButton.visible = false
             }
