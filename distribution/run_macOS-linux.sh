0bf046fbae48500fff59db2151cba4cb8453085d
chrislo27 <kinchrislo@gmail.com>
2019-02-18 14:43:50 -0800

    Remove you need Java 1.8+ warning

--- distribution/run_macOS-linux.sh
+++ distribution/run_macOS-linux.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 java -version
-
-printf "\nYou NEED Java 1.8, at least. If the version above is not 1.8+, or if you get an UnsupportedClassVersionError, update Java.\n\n"
-
 java -jar RHRE.jar
 
 read -n1 "Press any key to continue..."
