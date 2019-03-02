0fbd6a3e6181dc101c28ebefd14d9c6adeffe18d
chrislo27 <kinchrislo@gmail.com>
2019-03-01 17:35:08 -0800

    Picker display has less alpha when current tool is not selection

--- core/src/main/kotlin/io/github/chrislo27/rhre3/editor/stage/PickerDisplay.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/editor/stage/PickerDisplay.kt
@@ -2,6 +2,7 @@ package io.github.chrislo27.rhre3.editor.stage
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.MathUtils
@@ -12,7 +13,10 @@ import io.github.chrislo27.rhre3.screen.EditorScreen
 import io.github.chrislo27.toolboks.ui.Stage
 import io.github.chrislo27.toolboks.ui.UIElement
 import io.github.chrislo27.toolboks.ui.UIPalette
-import io.github.chrislo27.toolboks.util.gdxutils.*
+import io.github.chrislo27.toolboks.util.gdxutils.getTextWidth
+import io.github.chrislo27.toolboks.util.gdxutils.prepareStencilMask
+import io.github.chrislo27.toolboks.util.gdxutils.scaleMul
+import io.github.chrislo27.toolboks.util.gdxutils.useStencilMask
 import kotlin.math.absoluteValue
 
 
@@ -39,6 +43,7 @@ class PickerDisplay(val editor: Editor, val number: Int, val palette: UIPalette,
         }.useStencilMask {
             val font = editor.main.defaultBorderedFont
             val sectionY = location.realHeight / number
+            val currentTool = editor.currentTool
             labels.forEachIndexed { index, label ->
                 val half = number / 2
                 if ((scrollValue - index).absoluteValue > half + 1) {
@@ -46,13 +51,15 @@ class PickerDisplay(val editor: Editor, val number: Int, val palette: UIPalette,
                 }
 
                 val selected = index == selection.currentIndex
-                font.color = if (selected && editor.currentTool == Tool.SELECTION) Editor.SELECTED_TINT else label.color
+                font.color = if (selected && currentTool == Tool.SELECTION) Editor.SELECTED_TINT else label.color
                 val mainWidth = font.getTextWidth(label.main)
                 val availableWidth = location.realWidth
                 val drawX = location.realX
                 val drawY = location.realY + location.realHeight / 2 + sectionY * (scrollValue - index)
+                val shouldUseNewAlpha = currentTool != Tool.SELECTION
+                val newAlpha = 0.4f
                 if (label.sub.isEmpty()) {
-                    font.drawCompressed(batch, label.main, drawX, drawY + font.capHeight / 2, availableWidth, Align.left)
+                    drawTextWithAlpha(batch, font, label.main, drawX, drawY + font.capHeight / 2, availableWidth, Align.left, false, newAlpha, shouldUseNewAlpha)
                 } else {
                     val subScale = 0.75f
                     font.scaleMul(subScale)
@@ -64,15 +71,32 @@ class PickerDisplay(val editor: Editor, val number: Int, val palette: UIPalette,
                     val scaleFactor = if (totalWidth > availableWidth) availableWidth / totalWidth else 1f
                     val oldCapHeight = font.capHeight
                     font.data.scaleX = scaleFactor * oldScaleX
-                    font.draw(batch, label.main, drawX, drawY + oldCapHeight / 2, availableWidth, Align.left, false)
+                    drawTextWithAlpha(batch, font, label.main, drawX, drawY + oldCapHeight / 2, availableWidth, Align.left, false, newAlpha, shouldUseNewAlpha)
                     font.scaleMul(subScale)
-                    font.draw(batch, " " + label.sub, drawX + mainWidth * scaleFactor, drawY - oldCapHeight / 2 + font.capHeight, availableWidth, Align.left, false)
+                    drawTextWithAlpha(batch, font, " " + label.sub, drawX + mainWidth * scaleFactor, drawY - oldCapHeight / 2 + font.capHeight, availableWidth, Align.left, false, newAlpha, shouldUseNewAlpha)
                     font.data.setScale(oldScaleX, oldScaleY)
                 }
             }
 
             font.setColor(1f, 1f, 1f, 1f)
         }
+    }
+
+    private fun drawTextWithAlpha(batch: SpriteBatch, font: BitmapFont, text: String, drawX: Float, drawY: Float, availableWidth: Float, align: Int, wrap: Boolean, alpha: Float, setAlpha: Boolean) {
+        val textWidth = font.getTextWidth(text)
+        val oldScaleX = font.data.scaleX
+        if (textWidth > availableWidth) {
+            font.data.scaleX = (availableWidth / textWidth) * oldScaleX
+        }
+        font.cache.also { cache ->
+            cache.clear()
+            cache.addText(text, drawX, drawY + font.capHeight / 2, availableWidth, align, wrap)
+            if (setAlpha) {
+                cache.setAlphas(alpha)
+            }
+            cache.draw(batch)
+        }
+        font.data.scaleX = oldScaleX
     }
 
     data class Label(var main: String, var sub: String, var color: Color)
