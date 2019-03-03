32f9a0d04d7f8352f08d711e14ce7cc55fd538a6
chrislo27 <kinchrislo@gmail.com>
2019-03-02 21:05:40 -0800

    Gave backgrounds names

--- core/src/main/kotlin/io/github/chrislo27/rhre3/stage/bg/Background.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/stage/bg/Background.kt
@@ -11,19 +11,20 @@ abstract class Background(val id: String) {
     abstract fun render(camera: OrthographicCamera, batch: SpriteBatch, shapeRenderer: ShapeRenderer, delta: Float)
 
     companion object {
-        val backgrounds: List<Background> by lazy {
-            listOf(
-                    TengokuBackground("tengoku"),
-                    KarateManBackground("karateMan"),
-                    TilingBackground("rhdsPolkaDots", 5f, speedX = 3f, speedY = -3f, widthCoeff = 0.5f, heightCoeff = 0.5f) { AssetRegistry["bg_polkadot"] },
-                    SpaceDanceBackground("spaceDance"),
-                    RetroBackground("retro"),
-                    TilingBackground("tapTrial", 5f, speedX = 0f, speedY = 1f) { AssetRegistry["bg_tapTrial"] },
-                    TilingBackground("tiled", 5f, speedX = 1f, speedY = 1f) { AssetRegistry["bg_tile"] },
-                    LaunchPartyBackground("launchParty"),
-                    KittiesBackground("kitties")
+        val backgroundsNames: Map<Background, String> by lazy {
+            linkedMapOf(
+                    TengokuBackground("tengoku") to "Tengoku",
+                    KarateManBackground("karateMan") to "Karate Man",
+                    TilingBackground("rhdsPolkaDots", 5f, speedX = 3f, speedY = -3f, widthCoeff = 0.5f, heightCoeff = 0.5f) { AssetRegistry["bg_polkadot"] } to "RHDS",
+                    SpaceDanceBackground("spaceDance") to "Space Dance",
+                    RetroBackground("retro") to "Retro",
+                    TilingBackground("tapTrial", 5f, speedX = 0f, speedY = 1f) { AssetRegistry["bg_tapTrial"] } to "Tap Trial",
+                    TilingBackground("tiled", 5f, speedX = 1f, speedY = 1f) { AssetRegistry["bg_tile"] } to "Tiled",
+                    LaunchPartyBackground("launchParty") to "Launch Party",
+                    KittiesBackground("kitties") to "Kitties!"
                   )
         }
+        val backgrounds: List<Background> by lazy { backgroundsNames.keys.toList() }
         val backgroundMap: Map<String, Background> by lazy { backgrounds.associateBy(Background::id) }
         val defaultBackground: Background
             get() = backgroundMap.getValue("tengoku")
