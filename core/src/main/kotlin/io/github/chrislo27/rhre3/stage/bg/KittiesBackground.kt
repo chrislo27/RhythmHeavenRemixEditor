5a2dc7c0f18002419c57fd163a6ade66390f604b
chrislo27 <kinchrislo@gmail.com>
2019-03-08 18:31:58 -0800

    Fix Kitties! background colour

--- core/src/main/kotlin/io/github/chrislo27/rhre3/stage/bg/KittiesBackground.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/stage/bg/KittiesBackground.kt
@@ -1,5 +1,6 @@
 package io.github.chrislo27.rhre3.stage.bg
 
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
@@ -7,9 +8,15 @@ import io.github.chrislo27.toolboks.util.gdxutils.fillRect
 
 
 class KittiesBackground(id: String) : Background(id) {
+
+    companion object {
+        val BG_COLOUR: Color = Color.valueOf("FCFBFD")
+    }
+
     override fun render(camera: OrthographicCamera, batch: SpriteBatch, shapeRenderer: ShapeRenderer, delta: Float) {
-        batch.setColor(1f, 1f, 1f, 1f)
+        batch.color = BG_COLOUR
         batch.fillRect(0f, 0f, camera.viewportWidth, camera.viewportHeight)
+        batch.setColor(1f, 1f, 1f, 1f)
     }
 
 }
