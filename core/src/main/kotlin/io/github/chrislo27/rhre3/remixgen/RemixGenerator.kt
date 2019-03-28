8e1c212ce0f61648afc928f184eb85f7ee0f700b
chrislo27 <kinchrislo@gmail.com>
2019-03-28 15:55:28 -0700

    Add generate call

--- core/src/main/kotlin/io/github/chrislo27/rhre3/remixgen/RemixGenerator.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/remixgen/RemixGenerator.kt
@@ -6,6 +6,17 @@ import java.util.*
 
 class RemixGenerator(val remix: Remix, val settings: RemixGeneratorSettings) {
 
+    val rootBeat: Int = remix.timeSignatures.map.values.firstOrNull { it.divisions % 2 == 0 }?.beat ?: 0
+    val canGenerate: Boolean = remix.duration < Float.POSITIVE_INFINITY && remix.remixGeneratorSettings == null
     private val random: Random = Random(settings.seed)
+
+    /**
+     * Generates the remix. Calling this function multiple times leads to undefined behaviour.
+     */
+    fun generate() {
+        if (!canGenerate) return
+
+        remix.remixGeneratorSettings = settings.copy()
+    }
 
 }
--- core/src/main/kotlin/io/github/chrislo27/rhre3/screen/RemixGeneratorScreen.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/screen/RemixGeneratorScreen.kt
@@ -3,15 +3,16 @@ package io.github.chrislo27.rhre3.screen
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.utils.Align
 import io.github.chrislo27.rhre3.RHRE3Application
 import io.github.chrislo27.rhre3.editor.Editor
+import io.github.chrislo27.rhre3.registry.GameRegistry
+import io.github.chrislo27.rhre3.remixgen.RemixGenerator
+import io.github.chrislo27.rhre3.remixgen.RemixGeneratorSettings
 import io.github.chrislo27.rhre3.stage.GenericStage
 import io.github.chrislo27.rhre3.track.Remix
 import io.github.chrislo27.toolboks.ToolboksScreen
-import io.github.chrislo27.toolboks.registry.AssetRegistry
+import io.github.chrislo27.toolboks.i18n.Localization
 import io.github.chrislo27.toolboks.registry.ScreenRegistry
 import io.github.chrislo27.toolboks.ui.Button
 import io.github.chrislo27.toolboks.ui.TextField
@@ -25,11 +26,23 @@ class RemixGeneratorScreen(main: RHRE3Application, val editor: Editor)
 
     override val stage: GenericStage<RemixGeneratorScreen> = GenericStage(main.uiPalette, null, main.defaultCamera)
     private val seedField: TextField<RemixGeneratorScreen>
+    private val rootBeatLabel: TextLabel<RemixGeneratorScreen>
+    private val generateButton: Button<RemixGeneratorScreen>
+
+    private var settings: RemixGeneratorSettings = RemixGeneratorSettings(System.currentTimeMillis())
+        set(value) {
+            field = value
+            generator = RemixGenerator(remix, settings)
+            Gdx.app.postRunnable {
+                updateLabels()
+            }
+        }
+    private var generator: RemixGenerator = RemixGenerator(remix, settings)
 
     init {
         val palette = main.uiPalette
         stage.titleLabel.text = "screen.remixGen.title"
-        stage.titleIcon.image = TextureRegion(AssetRegistry.get<Texture>("ui_icon_inspections_big"))
+//        stage.titleIcon.image = TextureRegion(AssetRegistry.get<Texture>("ui_icon_inspections_big"))
         stage.backButton.visible = true
         stage.onBackButtonClick = {
             main.screen = ScreenRegistry.getNonNull("editor")
@@ -40,6 +53,13 @@ class RemixGeneratorScreen(main: RHRE3Application, val editor: Editor)
         val labelHeight = 0.125f
         val fieldHeight = 0.1f
 
+        rootBeatLabel = TextLabel(palette, stage.centreStage, stage.centreStage).apply {
+            this.textWrapping = false
+            this.isLocalizationKey = false
+            this.text = ""
+            this.location.set(screenHeight = labelHeight)
+        }
+        stage.centreStage.elements += rootBeatLabel
         stage.centreStage.elements += TextLabel(palette, stage.centreStage, stage.centreStage).apply {
             this.textWrapping = false
             this.text = "screen.remixGen.seed"
@@ -47,7 +67,12 @@ class RemixGeneratorScreen(main: RHRE3Application, val editor: Editor)
             this.textColor = Color.LIGHT_GRAY
             this.location.set(screenX = 0.25f, screenWidth = 0.5f, screenHeight = labelHeight, screenY = 0.8f - labelHeight * -0.5f)
         }
-        seedField = TextField(palette, stage.centreStage, stage.centreStage).apply {
+        seedField = object : TextField<RemixGeneratorScreen>(palette, stage.centreStage, stage.centreStage) {
+            override fun onTextChange(oldText: String) {
+                val longSeed = text.trim().toLongOrNull()
+                settings = settings.copy(seed = longSeed ?: text.hashCode().toLong())
+            }
+        }.apply {
             this.location.set(screenX = 0.25f, screenWidth = 0.5f, screenHeight = fieldHeight, screenY = 0.8f - labelHeight * 0.5f)
             this.background = true
         }
@@ -60,14 +85,33 @@ class RemixGeneratorScreen(main: RHRE3Application, val editor: Editor)
             this.location.set(screenX = 0.25f, screenWidth = 0.5f, screenHeight = labelHeight, screenY = 0.8f - labelHeight * 1.5f)
         }
 
-        stage.bottomStage.elements += object : Button<RemixGeneratorScreen>(palette, stage.bottomStage, stage.bottomStage){
-
+        generateButton = object : Button<RemixGeneratorScreen>(palette, stage.bottomStage, stage.bottomStage){
+            override fun onLeftClick(xPercent: Float, yPercent: Float) {
+                super.onLeftClick(xPercent, yPercent)
+                this.enabled = false
+                generator.generate()
+                main.screen = ScreenRegistry["editor"]
+            }
         }.apply {
             this.addLabel(TextLabel(palette, this, this.stage).apply {
                 this.text = "screen.remixGen.generate"
                 this.textWrapping = false
             })
             this.location.set(screenX = 0.2f, screenWidth = 0.6f)
+        }
+        stage.bottomStage.elements += generateButton
+
+        updateLabels()
+    }
+
+    fun updateLabels() {
+        generateButton.enabled = generator.canGenerate
+        rootBeatLabel.text = if (generator.canGenerate) Localization["screen.remixGen.rootBeat", generator.rootBeat.toString()] else {
+            if (remix.duration == Float.POSITIVE_INFINITY) {
+                Localization["screen.remixGen.missingEndRemix", GameRegistry.data.objectMap[GameRegistry.END_REMIX_ENTITY_ID]?.name]
+            } else {
+                Localization["screen.remixGen.alreadyGenerated"]
+            }
         }
     }
 
