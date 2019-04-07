66f96721f5aa0cb949b3fea5b53c2b09d39f225a
chrislo27 <kinchrislo@gmail.com>
2019-04-07 12:15:30 -0700

    Add JN playalong keybindings

--- core/src/main/kotlin/io/github/chrislo27/rhre3/playalong/PlayalongControls.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/playalong/PlayalongControls.kt
@@ -12,22 +12,27 @@ data class PlayalongControls(var buttonA: Int = Input.Keys.J,
                              var buttonDown: Int = Input.Keys.S) {
 
     companion object {
-        val QWERTY_WASD = PlayalongControls()
-        val AZERTY_ZSQD = PlayalongControls(buttonLeft = Input.Keys.Q, buttonRight = Input.Keys.D, buttonUp = Input.Keys.Z, buttonDown = Input.Keys.S)
+        val QWERTY_WASD_JK = PlayalongControls()
+        val AZERTY_ZSQD_JK = PlayalongControls(buttonLeft = Input.Keys.Q, buttonRight = Input.Keys.D, buttonUp = Input.Keys.Z, buttonDown = Input.Keys.S)
         val ARROW_KEYS_ZX = PlayalongControls(Input.Keys.Z, Input.Keys.X, Input.Keys.LEFT, Input.Keys.RIGHT, Input.Keys.UP, Input.Keys.DOWN)
         val ARROW_KEYS_WX = PlayalongControls(Input.Keys.W, Input.Keys.X, Input.Keys.LEFT, Input.Keys.RIGHT, Input.Keys.UP, Input.Keys.DOWN)
         val QWERTY_IJKL = PlayalongControls(Input.Keys.Z, Input.Keys.X, Input.Keys.J, Input.Keys.L, Input.Keys.I, Input.Keys.K)
         val AZERTY_IJKL = PlayalongControls(Input.Keys.W, Input.Keys.X, Input.Keys.J, Input.Keys.L, Input.Keys.I, Input.Keys.K)
+        val QWERTY_WASD_JN = PlayalongControls(buttonB = Input.Keys.N)
+        val AZERTY_ZSQD_JN = PlayalongControls(buttonB = Input.Keys.N, buttonLeft = Input.Keys.Q, buttonRight = Input.Keys.D, buttonUp = Input.Keys.Z, buttonDown = Input.Keys.S)
         val INVALID = PlayalongControls(-1, -1, -1, -1, -1, -1)
 
         val strCustom = "Custom"
         val standardControls: Map<String, PlayalongControls> = mapOf(
-                "QWERTY WASD" to QWERTY_WASD,
-                "AZERTY ZSQD" to AZERTY_ZSQD,
-                "QWERTY Arrow Keys" to ARROW_KEYS_ZX,
-                "AZERTY Arrow Keys" to ARROW_KEYS_WX,
-                "QWERTY IJKL" to QWERTY_IJKL,
-                "AZERTY IJKL" to AZERTY_IJKL
+                "QWERTY WASD/JK" to QWERTY_WASD_JK,
+                "QWERTY Arrow Keys/ZX" to ARROW_KEYS_ZX,
+                "QWERTY IJKL/ZX" to QWERTY_IJKL,
+                "QWERTY WASD/JN" to QWERTY_WASD_JN,
+
+                "AZERTY ZSQD/JK" to AZERTY_ZSQD_JK,
+                "AZERTY Arrow Keys/WX" to ARROW_KEYS_WX,
+                "AZERTY IJKL/WX" to AZERTY_IJKL,
+                "AZERTY ZSQD/JN" to AZERTY_ZSQD_JN
                                                                     )
     }
 
