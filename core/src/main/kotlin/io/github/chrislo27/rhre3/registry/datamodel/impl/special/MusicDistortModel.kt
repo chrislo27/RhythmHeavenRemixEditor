547afa42723913a2bdc096f8e1271ddd410c4584
chrislo27 <kinchrislo@gmail.com>
2019-04-07 21:24:03 -0700

    Add subtext to Music Distortion entity

--- core/src/main/kotlin/io/github/chrislo27/rhre3/registry/datamodel/impl/special/MusicDistortModel.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/registry/datamodel/impl/special/MusicDistortModel.kt
@@ -2,12 +2,15 @@ package io.github.chrislo27.rhre3.registry.datamodel.impl.special
 
 import io.github.chrislo27.rhre3.entity.model.special.MusicDistortEntity
 import io.github.chrislo27.rhre3.registry.Game
+import io.github.chrislo27.rhre3.registry.datamodel.PickerName
 import io.github.chrislo27.rhre3.registry.datamodel.impl.CuePointer
 import io.github.chrislo27.rhre3.track.Remix
 
 
 class MusicDistortModel(game: Game, id: String, deprecatedIDs: List<String>, name: String)
     : SpecialDatamodel(game, id, deprecatedIDs, name, 1f) {
+
+    override val pickerName: PickerName = super.pickerName.copy(sub = "[LIGHT_GRAY]Applies high & low pass filter on music[]")
 
     override fun gameValidityPredicate(game: Game): Boolean {
         return super.gameValidityPredicate(game) || game.id == "djSchool"
