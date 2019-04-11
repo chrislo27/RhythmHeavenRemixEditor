17c6aa99441870d44d3ea42a21da0d7883318935
chrislo27 <kinchrislo@gmail.com>
2019-04-10 21:30:38 -0700

    Fix Play-Yan's hops with non X/4 time signatures

--- core/src/main/kotlin/io/github/chrislo27/rhre3/editor/rendering/EditorRendering.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/editor/rendering/EditorRendering.kt
@@ -35,11 +35,12 @@ fun Editor.renderPlayYan(batch: SpriteBatch) {
                    step * 26, 0, 26, 35, false, false)
     }
     if (remix.playState != PlayState.STOPPED) {
-        val beatPercent = beat % 1f
-        val playbackStartPercent = remix.playbackStart % 1f
+        val interval = remix.timeSignatures.getTimeSignature(beat)?.noteFraction ?: 1f
+        val beatPercent = beat % interval
+        val playbackStartPercent = remix.playbackStart % interval
         val floorPbStart = Math.floor(playbackStartPercent.toDouble()).toFloat()
         val currentSwing = remix.tempos.swingAt(beat)
-        val jumpHeight: Float = MathUtils.sin(MathUtils.PI * (if (playbackStartPercent > 0f && remix.beat < floorPbStart + 1f) (beat - remix.playbackStart) / (1f - remix.playbackStart % 1f) else beatPercent)).absoluteValue
+        val jumpHeight: Float = MathUtils.sin(MathUtils.PI / interval * (if (playbackStartPercent > 0f && remix.beat < floorPbStart + 1f) (beat - remix.playbackStart) / (1f - playbackStartPercent) else beatPercent)).absoluteValue
 
         batch.draw(AssetRegistry.get<Texture>(if (currentSwing.ratio == 50) "playyan_jumping" else "playyan_pogo"), beat,
                    remix.trackCount + 1f * jumpHeight, toScaleX(26f), toScaleY(35f),
