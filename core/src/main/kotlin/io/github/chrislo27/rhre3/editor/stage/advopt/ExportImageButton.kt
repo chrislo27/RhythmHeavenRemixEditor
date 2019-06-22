53ca94f2c1661e4c89ad7ca744a18d7dd8a5c181
chrislo27 <kinchrislo@gmail.com>
2019-06-22 00:22:30 -0700

    Add scaling parameter (not used)

--- core/src/main/kotlin/io/github/chrislo27/rhre3/editor/stage/advopt/ExportImageButton.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/editor/stage/advopt/ExportImageButton.kt
@@ -62,15 +62,16 @@ class ExportImageButton(val editor: Editor, palette: UIPalette, parent: UIElemen
             val duration = remix.duration
             if (file != null && duration > 0f) {
                 Gdx.app.postRunnable {
-                    val singleRowWidth = (duration + 1) * Editor.ENTITY_WIDTH
-                    val singleRowHeight = ((remix.trackCount + 4) * Editor.ENTITY_HEIGHT)
+                    val scale = 1f // Very large scales may cause errors when initing the pixmap
+                    val singleRowWidth = (duration + 1) * Editor.ENTITY_WIDTH * scale
+                    val singleRowHeight = ((remix.trackCount + 4) * Editor.ENTITY_HEIGHT) * scale
                     val rowEstimate = (sqrt(singleRowWidth / singleRowHeight).roundToInt()).coerceAtLeast(1)
                     val rows = if (wasShiftHeld) 1 else rowEstimate
-                    val pixmap = Pixmap(((duration + 1) * Editor.ENTITY_WIDTH / rows).roundToInt(), singleRowHeight.roundToInt() * rows, Pixmap.Format.RGBA8888)
+                    val pixmap = Pixmap((singleRowWidth / rows).roundToInt(), singleRowHeight.roundToInt() * rows, Pixmap.Format.RGBA8888)
 
                     val oldCamX = editor.camera.position.x
                     val oldCamZoom = editor.camera.zoom
-                    val buffer = FrameBuffer(Pixmap.Format.RGB888, RHRE3.WIDTH, RHRE3.HEIGHT, false, true)
+                    val buffer = FrameBuffer(Pixmap.Format.RGB888, (RHRE3.WIDTH * scale).roundToInt(), (RHRE3.HEIGHT * scale).roundToInt(), false, true)
                     val wasTextOnScreen = ModelEntity.attemptTextOnScreen
                     ModelEntity.attemptTextOnScreen = false
 
@@ -78,9 +79,9 @@ class ExportImageButton(val editor: Editor, palette: UIPalette, parent: UIElemen
 
                     Toolboks.LOGGER.debug("Pixmap size: ${pixmap.width} by ${pixmap.height} with $rows rows")
 
-                    val stepValue = (editor.camera.viewportWidth * Editor.ENTITY_WIDTH).roundToInt()
+                    val stepValue = (editor.camera.viewportWidth * Editor.ENTITY_WIDTH * scale).roundToInt()
                     for (x in 0..(pixmap.width * rows) step stepValue) {
-                        editor.camera.position.x = x / Editor.ENTITY_WIDTH + editor.camera.viewportWidth / 2f - 0.5f
+                        editor.camera.position.x = x / (Editor.ENTITY_WIDTH * scale) + editor.camera.viewportWidth / 2f - 0.5f
                         editor.camera.zoom = 1f
                         editor.camera.update()
 
@@ -88,12 +89,12 @@ class ExportImageButton(val editor: Editor, palette: UIPalette, parent: UIElemen
 
                         val bufPix = ScreenUtils.getFrameBufferPixmap(0, 0, buffer.width, buffer.height)
                         val currentRow = x / (pixmap.width)
-                        pixmap.drawPixmap(bufPix, x % (pixmap.width), (rows - 1 - currentRow) * (pixmap.height / rows), 0, (Editor.ENTITY_HEIGHT * 4.5f).roundToInt(), bufPix.width, pixmap.height / rows)
+                        pixmap.drawPixmap(bufPix, x % (pixmap.width), (rows - 1 - currentRow) * (pixmap.height / rows), 0, (Editor.ENTITY_HEIGHT * 4.5f * scale).roundToInt(), bufPix.width, pixmap.height / rows)
                         // if the end of this section spans into the next row, render that section too
                         val endRow = (x + stepValue) / (pixmap.width)
                         if (endRow > currentRow) {
                             Toolboks.LOGGER.debug("Stepping into next row: $currentRow -> $endRow")
-                            pixmap.drawPixmap(bufPix, (x + stepValue) % (pixmap.width) - stepValue, (rows - 1 - endRow) * (pixmap.height / rows), 0, (Editor.ENTITY_HEIGHT * 4.5f).roundToInt(), bufPix.width, pixmap.height / rows)
+                            pixmap.drawPixmap(bufPix, (x + stepValue) % (pixmap.width) - stepValue, (rows - 1 - endRow) * (pixmap.height / rows), 0, (Editor.ENTITY_HEIGHT * 4.5f * scale).roundToInt(), bufPix.width, pixmap.height / rows)
                         }
                         bufPix.dispose()
 
