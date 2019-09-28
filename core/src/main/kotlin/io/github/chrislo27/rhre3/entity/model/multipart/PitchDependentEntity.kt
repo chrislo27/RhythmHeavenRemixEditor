b25d5e2a80ff29fc64ef34f08aa6d26e74f4a4cd
chrislo27 <kinchrislo@gmail.com>
2019-09-27 17:07:25 -0700

    Make pitch dependent stretchable

--- core/src/main/kotlin/io/github/chrislo27/rhre3/entity/model/multipart/PitchDependentEntity.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/entity/model/multipart/PitchDependentEntity.kt
@@ -4,10 +4,12 @@ import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.math.Rectangle
 import io.github.chrislo27.rhre3.editor.Editor
 import io.github.chrislo27.rhre3.entity.model.ILoadsSounds
+import io.github.chrislo27.rhre3.entity.model.IStretchable
 import io.github.chrislo27.rhre3.entity.model.IVolumetric
 import io.github.chrislo27.rhre3.entity.model.MultipartEntity
 import io.github.chrislo27.rhre3.sfxdb.SFXDatabase
 import io.github.chrislo27.rhre3.sfxdb.datamodel.Datamodel
+import io.github.chrislo27.rhre3.sfxdb.datamodel.impl.Cue
 import io.github.chrislo27.rhre3.sfxdb.datamodel.impl.CuePointer
 import io.github.chrislo27.rhre3.sfxdb.datamodel.impl.PitchDependent
 import io.github.chrislo27.rhre3.sfxdb.datamodel.impl.PitchRange
@@ -16,27 +18,29 @@ import io.github.chrislo27.rhre3.track.Remix
 
 
 class PitchDependentEntity(remix: Remix, datamodel: PitchDependent)
-    : MultipartEntity<PitchDependent>(remix, datamodel) {
-    
+    : MultipartEntity<PitchDependent>(remix, datamodel), IStretchable {
+
+    override val isStretchable: Boolean = datamodel.cues.mapNotNull { SFXDatabase.data.objectMap[it.id] }.any { it is Cue && it.stretchable }
+
     init {
         bounds.width = datamodel.cues.map(CuePointer::duration).max() ?: error(
                 "PitchDependent datamodel ${datamodel.id} has no internal cues")
     }
-    
+
     private fun getPossibleObjects(): List<Triple<PitchRange, Datamodel, CuePointer>> {
         return datamodel.intervals.mapNotNull { (interval, pointer) ->
             SFXDatabase.data.objectMap[pointer.id]?.let { Triple(interval, it, pointer) }
         }
     }
-    
+
     private fun pickSound() {
         val thisSemitone = semitone
         val thisVolume = volumePercent
-        
+
         // Set semitone and volume to zero, repopulate, and reset it so the setters in MultipartEntity take effect
         semitone = 0
         volumePercent = IVolumetric.DEFAULT_VOLUME
-        
+
         internal.clear()
         val possible = getPossibleObjects()
         if (possible.isEmpty()) {
@@ -52,16 +56,16 @@ class PitchDependentEntity(remix: Remix, datamodel: PitchDependent)
                         }
                     }
                 }
-        
+
         // Re-set semitone and volume so it takes effect in the internals
         semitone = thisSemitone
         volumePercent = thisVolume
     }
-    
+
     override fun getRenderColor(editor: Editor, theme: Theme): Color {
         return theme.entities.pattern
     }
-    
+
     override fun loadSounds() {
         super.loadSounds()
         getPossibleObjects()
@@ -72,7 +76,7 @@ class PitchDependentEntity(remix: Remix, datamodel: PitchDependent)
                     }
                 }
     }
-    
+
     override fun unloadSounds() {
         super.unloadSounds()
         getPossibleObjects()
@@ -83,18 +87,18 @@ class PitchDependentEntity(remix: Remix, datamodel: PitchDependent)
                     }
                 }
     }
-    
+
     override fun updateInternalCache(oldBounds: Rectangle) {
-        translateInternal(oldBounds)
+        translateInternal(oldBounds, changeWidths = true, scaleBeats = true)
         if (internal.isEmpty())
             pickSound()
     }
-    
+
     override fun onStart() {
         pickSound()
         super.onStart()
     }
-    
+
     override fun copy(remix: Remix): PitchDependentEntity {
         return PitchDependentEntity(remix, datamodel).also {
             it.updateBounds {
