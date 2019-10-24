d728e455bb8e671bc62f12e37801608207366d18
chrislo27 <kinchrislo@gmail.com>
2019-10-24 16:04:56 -0700

    Refactor background list

--- core/src/main/kotlin/io/github/chrislo27/rhre3/stage/bg/Background.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/stage/bg/Background.kt
@@ -8,32 +8,36 @@ import io.github.chrislo27.toolboks.registry.AssetRegistry
 
 
 abstract class Background(val id: String) {
-
+    
     abstract fun render(camera: OrthographicCamera, batch: SpriteBatch, shapeRenderer: ShapeRenderer, delta: Float)
-
+    
     companion object {
-        val backgroundsNames: Map<Background, String> by lazy {
-            linkedMapOf(
-                    TengokuBackground("tengoku") to "Tengoku",
-                    KarateManBackground("karateMan") to "Karate Man",
-                    TilingBackground("rhdsPolkaDots", 5f, speedX = 3f, speedY = -3f, widthCoeff = 0.5f, heightCoeff = 0.5f) { AssetRegistry["bg_polkadot"] } to "DS Game Select",
-                    SpaceDanceBackground("spaceDance") to "Space Dance",
-                    RetroBackground("retro") to "Retro",
-                    TilingBackground("tapTrial", 5f, speedX = 0f, speedY = 1f) { AssetRegistry["bg_tapTrial"] } to "Tap Trial",
-                    TilingBackground("tiled", 5f, speedX = 1f, speedY = 1f) { AssetRegistry["bg_tile"] } to "Notes",
-                    LaunchPartyBackground("launchParty") to "Launch Party",
-                    KittiesBackground("kitties") to "Kitties!",
-                    SeesawBackground("seesaw") to "See-Saw",
-                    KarateManStripesBackground("karateManStripes1", stripe1 = Color.valueOf("FEC652"),
-                    stripe2 = Color.valueOf("FFE86C")) to "Karate Man GBA 1",
-                    KarateManStripesBackground("karateManStripes2") to "Karate Man GBA 2",
-                    BTSDSBackground("btsDS") to "Built to Scale DS"
+        data class BgData(val bg: Background, val name: String)
+        
+        val backgroundsData: List<BgData> by lazy {
+            listOf(
+                    BgData(TengokuBackground("tengoku"), "Tengoku"),
+                    BgData(KarateManBackground("karateMan"), "Karate Man"),
+                    BgData(TilingBackground("rhdsPolkaDots", 5f,
+                                            speedX = 3f, speedY = -3f, widthCoeff = 0.5f, heightCoeff = 0.5f)
+                           { AssetRegistry["bg_polkadot"] }, "DS Game Select"),
+                    BgData(SpaceDanceBackground("spaceDance"), "Space Dance"),
+                    BgData(RetroBackground("retro"), "Retro"),
+                    BgData(TilingBackground("tapTrial", 5f, speedX = 0f, speedY = 1f) { AssetRegistry["bg_tapTrial"] }, "Tap Trial"),
+                    BgData(TilingBackground("tiled", 5f, speedX = 1f, speedY = 1f) { AssetRegistry["bg_tile"] }, "Notes"),
+                    BgData(LaunchPartyBackground("launchParty"), "Launch Party"),
+                    BgData(KittiesBackground("kitties"), "Kitties!"),
+                    BgData(SeesawBackground("seesaw"), "See-Saw"),
+                    BgData(KarateManStripesBackground("karateManStripes1", stripe1 = Color.valueOf("FEC652"),
+                                                      stripe2 = Color.valueOf("FFE86C")), "Karate Man GBA"),
+                    BgData(KarateManStripesBackground("karateManStripes2"), "Karate Man GBA 2"),
+                    BgData(BTSDSBackground("btsDS"), "Built to Scale DS")
                   )
         }
-        val backgrounds: List<Background> by lazy { backgroundsNames.keys.toList() }
+        val backgrounds: List<Background> by lazy { backgroundsData.map { it.bg } }
         val backgroundMap: Map<String, Background> by lazy { backgrounds.associateBy(Background::id) }
         val defaultBackground: Background
             get() = backgroundMap.getValue("tengoku")
     }
-
+    
 }
