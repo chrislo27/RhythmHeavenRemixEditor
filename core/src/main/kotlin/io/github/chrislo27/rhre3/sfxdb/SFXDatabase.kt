bec65382f09807192b08e85abcb5a7c847b22c31
chrislo27 <kinchrislo@gmail.com>
2020-04-08 21:27:23 -0700

    Add more search keywords to Playalong Input Entities

--- core/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/SFXDatabase.kt
+++ core/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/SFXDatabase.kt
@@ -542,7 +542,7 @@ object SFXDatabase : Disposable {
             val playalongGame = Game(PLAYALONG_GAME_ID, "Playalong Input Entities", specialGame.series,
                                      playalongObjs, Gdx.files.internal("images/gameicon/playable_entities.png"),
                                      null, "Special Entities", false, specialGame.priority,
-                                     false, specialGame.noDisplay, listOf("playable"), false, true)
+                                     false, specialGame.noDisplay, listOf("playable", "playing", "play-along"), false, true)
             checkGameDoesNotExist(PLAYALONG_GAME_ID)
             // Press
             playalongObjs += PlayalongModel(playalongGame, "${playalongGame.id}_press_A", listOf(),
