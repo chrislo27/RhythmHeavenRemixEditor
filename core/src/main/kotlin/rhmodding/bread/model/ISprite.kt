e3f1d4c72f007dcdae46bd75bd6ecc3df918ca23
chrislo27 <kinchrislo@gmail.com>
2020-05-13 21:38:25 -0700

    Remove JavaFX references in ISprite

--- core/src/main/kotlin/rhmodding/bread/model/ISprite.kt
+++ core/src/main/kotlin/rhmodding/bread/model/ISprite.kt
@@ -1,13 +1,5 @@
 package rhmodding.bread.model
 
-import javafx.scene.canvas.Canvas
-import javafx.scene.canvas.GraphicsContext
-import javafx.scene.transform.Affine
-import javafx.scene.transform.Rotate
-import javafx.scene.transform.Scale
-import kotlin.math.absoluteValue
-import kotlin.math.sign
-
 
 interface ISprite {
     
@@ -39,21 +31,21 @@ interface ISpritePart {
     
     fun copy(): ISpritePart
     
-    fun transform(canvas: Canvas, g: GraphicsContext) {
-        g.globalAlpha *= opacity.toInt() / 255.0
-        g.transform(Affine().apply {
-            append(Scale(stretchX.sign * 1.0, stretchY.sign * 1.0, posX - canvas.width / 2, posY - canvas.height / 2))
-            val pivotX = posX - canvas.width / 2 + regionW.toInt() * stretchX.absoluteValue * 0.5
-            val pivotY = posY - canvas.height / 2 + regionH.toInt() * stretchY.absoluteValue * 0.5
-            append(Rotate(rotation * stretchX.sign * stretchY.sign * 1.0, pivotX, pivotY))
-            if (flipX) {
-                append(Scale(-1.0, 1.0, pivotX, pivotY))
-            }
-            if (flipY) {
-                append(Scale(1.0, -1.0, pivotX, pivotY))
-            }
-        })
-    }
+//    fun transform(canvas: Canvas, g: GraphicsContext) {
+//        g.globalAlpha *= opacity.toInt() / 255.0
+//        g.transform(Affine().apply {
+//            append(Scale(stretchX.sign * 1.0, stretchY.sign * 1.0, posX - canvas.width / 2, posY - canvas.height / 2))
+//            val pivotX = posX - canvas.width / 2 + regionW.toInt() * stretchX.absoluteValue * 0.5
+//            val pivotY = posY - canvas.height / 2 + regionH.toInt() * stretchY.absoluteValue * 0.5
+//            append(Rotate(rotation * stretchX.sign * stretchY.sign * 1.0, pivotX, pivotY))
+//            if (flipX) {
+//                append(Scale(-1.0, 1.0, pivotX, pivotY))
+//            }
+//            if (flipY) {
+//                append(Scale(1.0, -1.0, pivotX, pivotY))
+//            }
+//        })
+//    }
     
 //    fun prepareForRendering(subimage: Image, multColor: Color, graphics: GraphicsContext): Image
     
